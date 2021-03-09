3fd10ff084315d23e9dc5c496f72c8864f20faa1
xueqingxiao <xueqingxiao@agora.io>
2021-03-09 16:45:31 +0800

    feat(component): icon

---
+++ packages/agora-scenario-ui-kit/src/components/icon/index.tsx
@@ -0,0 +1,33 @@
+import React, { FC } from 'react';
+import classnames from 'classnames';
+import { BaseProps } from '~components/interface/base-props';
+import { IconTypes } from './icon-types';
+import './index.css';
+
+export interface IconProps extends BaseProps {
+  type: IconTypes;
+  size?: number;
+  color?: string;
+}
+export const Icon: FC<IconProps> = ({
+  type,
+  className,
+  style,
+  size = 24,
+  color = '#333',
+  ...restProps
+}) => {
+  const cls = classnames({
+    [`iconfont icon-${type}`]: true,
+  });
+  return (
+    <i
+      className={cls}
+      style={{
+        color,
+        fontSize: size,
+        ...style,
+      }}
+      {...restProps}></i>
+  );
+};
