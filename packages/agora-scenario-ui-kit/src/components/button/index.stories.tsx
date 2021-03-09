53df13315ba55733b9b6544ccef7d6300cb6045c
matrixbirds <yooobuntu@gmail.com>
2021-03-09 16:14:10 +0800

    chore. add btn stories

--- packages/agora-scenario-ui-kit/src/components/button/index.stories.tsx
+++ packages/agora-scenario-ui-kit/src/components/button/index.stories.tsx
@@ -30,10 +30,10 @@ export const Docs = ({primary, secondary, ghost, danger}: DocsProps) => (
     </div>
     <h5>Disabled</h5>
     <div className="my-4">
-      <Button size="lg" disabled>Primary</Button>
-      <Button type="danger" size="lg" disabled>
-        Danger
-      </Button>
+      <Button size="lg" disabled>{primary}</Button>
+      <Button type="secondary" size="lg" disabled>{secondary}</Button>
+      <Button type="ghost" size="lg" disabled>{ghost}</Button>
+      <Button type="danger" size="lg" disabled>{danger}</Button>
     </div>
   </>
 );
