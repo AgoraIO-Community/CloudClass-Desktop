54fe2e863488091f26dd9155e29b154053c6911c
qianfengg <fred_gqf0221@163.com>
2021-03-10 10:02:29 +0000

    feat: use rc-notification to show toast
    feat: add story book controls select

--- packages/agora-scenario-ui-kit/src/components/toast/index.stories.tsx
+++ packages/agora-scenario-ui-kit/src/components/toast/index.stories.tsx
@@ -1,19 +1,32 @@
 import React from 'react';
 import { Meta } from '@storybook/react';
 import { Toast } from '~components/toast';
+import { Button } from '~components/button'
+
+import Notification from 'rc-notification'
 
 const meta: Meta = {
   title: 'Components/Toast',
   component: Toast,
+  argTypes: {
+    toastType: {
+      control: {
+        type: 'select',
+        options: ['success', 'error', 'warning']
+      }
+    }
+  }
 };
 
 type DocsProps = {
   success: string,
   error: string,
-  warning: string
+  warning: string,
+  toastText: string,
+  toastType: any,
 }
 
-export const Docs = ({success, error, warning}: DocsProps) => (
+export const Docs = ({success, error, warning, toastText, toastType = 'success'}: DocsProps) => (
   <>
     <div>
       <Toast>{success}</Toast>
@@ -24,13 +37,30 @@ export const Docs = ({success, error, warning}: DocsProps) => (
     <div className="mt-4">
       <Toast type="warning">{warning}</Toast>
     </div>
+    <div className="mt-4">
+      <Button size="lg" onClick={() => {
+        Notification.newInstance({}, notification => {
+          notification.notice({
+            content: <Toast type={toastType}>{toastText}</Toast>,
+            style: {
+              position: 'fixed',
+              top: '5%',
+              left: '50%',
+              transform: 'translateX(-50%)'
+            }
+          });
+        });
+      }}>show toast</Button>
+    </div>
   </>
 );
 
 Docs.args = {
   success: 'success',
   error: 'error',
-  warning: 'warning'
+  warning: 'warning',
+  toastText: 'test',
+
 }
 
 export default meta;
