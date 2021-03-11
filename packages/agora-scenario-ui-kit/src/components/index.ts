dbd8e80ef4f00828e29e8f006eab1208536af0f5
xueqingxiao <xueqingxiao@agora.io>
2021-03-11 16:31:04 +0800

    chore(components): rexport

--- packages/agora-scenario-ui-kit/src/components/index.ts
+++ packages/agora-scenario-ui-kit/src/components/index.ts
@@ -1,15 +1,9 @@
-<<<<<<< HEAD
 export * from './biz-header';
 export * from './button';
 export * from './chat';
 export * from './icon';
 export * from './layout';
 export * from './styles';
-export * from './video-player';
-=======
-export * from './button'
-export * from './layout'
-export * from './styles'
 export * from './toast'
 export * from './modal'
->>>>>>> 832641b50806f794a0ecc0d761539c3aab348ffb
+export * from './video-player';
