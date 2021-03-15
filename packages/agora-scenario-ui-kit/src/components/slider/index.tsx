1fb7606911f281e0dc047b74cdbabf8860ee59a7
qianfengg <fred_gqf0221@163.com>
2021-03-15 20:21:42 +0800

    feat: slider-tooltip

--- packages/agora-scenario-ui-kit/src/components/slider/index.tsx
+++ packages/agora-scenario-ui-kit/src/components/slider/index.tsx
@@ -2,9 +2,11 @@ import React, { FC } from 'react';
 import classnames from 'classnames';
 import { BaseProps } from '~components/interface/base-props';
 
-import RCSlider from 'rc-slider';
+import RCSlider, { SliderTooltip } from 'rc-slider';
 import 'rc-slider/assets/index.css';
 import './index.css';
+
+const { Handle } = RCSlider
 
 export interface SliderProps extends BaseProps {
     defaultValue: number;
@@ -12,9 +14,24 @@ export interface SliderProps extends BaseProps {
     max?: number;
     min?: number;
     step?: number;
-    tooltipPlacement?: string;
     onChange?: (value: number) => void;
 }
+
+const handle = (props: any) => {
+    const { value, dragging, index, ...restProps } = props;
+    return (
+        <SliderTooltip
+            prefixCls="rc-slider-tooltip"
+            overlay={`${value}`}
+            visible={dragging}
+            placement="top"
+            key={index}
+            overlayClassName={'agora-slider-tooltip'}
+        >
+            <Handle value={value} {...restProps} />
+        </SliderTooltip>
+    );
+};
 
 export const Slider: FC<SliderProps> = ({
     defaultValue = 0,
@@ -22,7 +39,6 @@ export const Slider: FC<SliderProps> = ({
     max = 100,
     min = 0,
     step = 1,
-    tooltipPlacement = 'tooltipPlacement',
     onChange = (value: number) => { console.log(value) },
     className,
     ...restProps
@@ -40,6 +56,7 @@ export const Slider: FC<SliderProps> = ({
                 min={min}
                 step={step}
                 onChange={onChange}
+                handle={handle}
             />
         </div>
     )
