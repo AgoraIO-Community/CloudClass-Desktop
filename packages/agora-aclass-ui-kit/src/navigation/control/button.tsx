63bc5827a76ca1e2e10b13edcd7193462549b34c
Qianze Zhang <qianze.zhang@hotmail.com>
2021-03-17 09:48:54 +0000

    fix lang
    fix dev failure
    
    add sos button

--- packages/agora-aclass-ui-kit/src/navigation/control/button.tsx
+++ packages/agora-aclass-ui-kit/src/navigation/control/button.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import IconButton from '@material-ui/core/IconButton'
 import customerService from '../assets/customerService.png'
+import sos from '../assets/sos.png'
 import equipmentDetection from '../assets/equipmentDetection.png'
 import refresh from '../assets/refresh.png'
 import userNetwork from '../assets/userNetwork.png'
@@ -27,7 +28,7 @@ export interface IExitButton {
 }
 
 const buttonsMap = {
-  'sos': (props: any) => <CustomizeIconBtn icon={customerService} style={{ ...defaultStyle, ...props.style }} />,
+  'sos': (props: any) => <CustomizeIconBtn icon={sos} style={{ ...defaultStyle, ...props.style }} />,
   'customerService': (props: any) => <CustomizeIconBtn icon={customerService} style={{ ...defaultStyle, ...props.style }} />,
   'equipmentDetection': (props: any) => <CustomizeIconBtn icon={equipmentDetection} style={{ ...defaultStyle, ...props.style }} />,
   'refresh': (props: any) => <CustomizeIconBtn icon={refresh} style={{ ...defaultStyle, ...props.style }} />,
