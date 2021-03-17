edffb12ee68d919ec6c42002751ef9bc256e420c
qianfengg <fred_gqf0221@163.com>
2021-03-17 18:14:38 +0800

    fix: conflict

--- packages/agora-scenario-ui-kit/src/components/device-manager/pretest/index.stories.tsx
+++ packages/agora-scenario-ui-kit/src/components/device-manager/pretest/index.stories.tsx
@@ -15,19 +15,11 @@ type DocsProps = {
     microphoneError: boolean; // 展示麦克风错误信息
 }
 
-<<<<<<< HEAD
 const cameraList = [...'.'.repeat(3)].map((item, index) => ({ label: '摄像头' + (index + 1), deviceId: 'camera-' + (index + 1) }))
 const microphoneList = [...'.'.repeat(3)].map((item, index) => ({ label: '麦克风' + (index + 1), deviceId: 'microphone-' + (index + 1) }))
 const speakerList = [...'.'.repeat(3)].map((item, index) => ({ label: '扬声器' + (index + 1), deviceId: 'speaker-' + (index + 1) }))
 
 export const Docs = ({ isNative, cameraError, microphoneError }: DocsProps) => (
-=======
-const cameraArray = [...'.'.repeat(3)].map((item, index) => ({ label: '摄像头' + (index + 1), deviceId: 'camera-' + (index + 1) }))
-const microphoneArray = [...'.'.repeat(3)].map((item, index) => ({ label: '麦克风' + (index + 1), deviceId: 'microphone-' + (index + 1) }))
-const speakerArray = [...'.'.repeat(3)].map((item, index) => ({ label: '扬声器' + (index + 1), deviceId: 'speaker-' + (index + 1) }))
-
-export const Docs = ({isNative, cameraError, microphoneError}: DocsProps) => (
->>>>>>> 25f7a75f6f649485513d24f0b6026887136e3266
     <>
         <div className="mt-4">
             <Modal
@@ -36,22 +28,15 @@ export const Docs = ({isNative, cameraError, microphoneError}: DocsProps) => (
                 footer={[<Button>完成检测</Button>]}
             >
                 <Pretest
-<<<<<<< HEAD
                     cameraList={cameraList}
                     microphoneList={microphoneList}
                     speakerList={speakerList}
-=======
-                    cameraArray={cameraArray}
-                    microphoneArray={microphoneArray}
-                    speakerArray={speakerArray}
->>>>>>> 25f7a75f6f649485513d24f0b6026887136e3266
                     isNative={isNative}
                     cameraError={cameraError}
                     microphoneError={microphoneError}
                 />
             </Modal>
         </div>
-<<<<<<< HEAD
         <div className="mt-4">
             <Button onClick={() => {
                 Modal.show({
@@ -71,8 +56,6 @@ export const Docs = ({isNative, cameraError, microphoneError}: DocsProps) => (
                 })
             }}>show pretest</Button>
         </div>
-=======
->>>>>>> 25f7a75f6f649485513d24f0b6026887136e3266
     </>
 )
 
