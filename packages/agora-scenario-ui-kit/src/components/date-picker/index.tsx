e81e9ea8908bb4510fb2cd94310cab8e5a8406c5
Qianze Zhang <qianze.zhang@hotmail.com>
2021-03-22 02:36:40 +0800

    date-picker

---
+++ packages/agora-scenario-ui-kit/src/components/date-picker/index.tsx
@@ -0,0 +1,68 @@
+import React, { EventHandler, FC, forwardRef, SyntheticEvent, useState } from 'react';
+import classnames from 'classnames';
+import { BaseProps } from '~components/interface/base-props';
+import './index.css';
+import RcDatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.min.css'
+import dayjs from 'dayjs'
+
+export interface DatePickerProps extends BaseProps {
+  inputClassName?: string
+  calendarClassName?: string
+}
+
+export const DatePicker: FC<DatePickerProps> = ({
+  inputClassName,
+  calendarClassName,
+  ...restProps
+}) => {
+  const inputcls = classnames({
+    [`${inputClassName}`]: !!inputClassName,
+  });
+  const calendarcls = classnames({
+    [`${calendarClassName}`]: !!calendarClassName,
+  });
+  const [startDate, setStartDate] = useState(new Date());
+  const [isOpen, setOpen] = useState(false);
+
+
+  return (
+    <div className="ag-calendar-container">
+      <RcDatePicker
+        selected={startDate}
+        onChange={date => setStartDate(date)}
+        // showTimeSelect
+        className={inputcls}
+        calendarClassName={calendarcls}
+        inline={true}
+        renderCustomHeader={({
+          date,
+          changeYear,
+          changeMonth,
+          decreaseMonth,
+          increaseMonth,
+          prevMonthButtonDisabled,
+          nextMonthButtonDisabled
+        }) => (
+          <div className="ag-calendar-header">
+            {dayjs(date).format('YYYY年MM月')}
+          </div>
+        )}
+        // customTimeInput={<ExampleCustomTimeInput></ExampleCustomTimeInput>}
+      />
+      <div className="ag-calendar-time-select-container">
+        <div className="ag-calendar-hour-select ag-calendar-time-select">
+          {[...Array(12).keys()].map(idx => <div className="ag-calendar-time-item">{`${idx}`.padStart(2, '0')}</div>)}
+        </div>
+        <div className="ag-calendar-minutes-select ag-calendar-time-select">
+          {[...Array(60).keys()].map(idx => <div className="ag-calendar-time-item">{`${idx}`.padStart(2, '0')}</div>)}
+        </div>
+      </div>
+      <div className="ag-calendar-ampm-select ag-calendar-time-select">
+        {[...Array(2).keys()].map(idx => <div className="ag-calendar-time-item">{idx === 0 ? '上午' : '下午'}</div>)}
+      </div>
+    </div>
+    
+  );
+};
+
--- packages/agora-scenario-ui-kit/src/components/select/index.tsx
+++ packages/agora-scenario-ui-kit/src/components/select/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, useRef } from 'react';
 import classnames from 'classnames';
 import { BaseProps } from '~components/interface/base-props';
 import RcSelect, { Option } from 'rc-select';
