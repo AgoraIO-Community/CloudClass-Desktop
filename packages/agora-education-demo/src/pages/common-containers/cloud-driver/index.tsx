333d2bf8753af29122acadecd1f1500796c3ae74
qianfengg <fred_gqf0221@163.com>
2021-03-22 21:31:16 +0800

    feat: upload file

--- packages/agora-education-demo/src/pages/common-containers/cloud-driver/index.tsx
+++ packages/agora-education-demo/src/pages/common-containers/cloud-driver/index.tsx
@@ -1,30 +1,71 @@
 import { useBoardStore } from '@/hooks'
 import { EduLogger } from 'agora-rte-sdk'
-import { Icon, Tabs, TabPane, Row, Button, CloudDisk, t } from 'agora-scenario-ui-kit'
+import { Icon, Tabs, TabPane, Row, Button, CloudDisk, t, Modal, Loading, Toast } from 'agora-scenario-ui-kit'
 import MD5 from 'js-md5'
 import { observer } from 'mobx-react'
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
 import { PPTKind } from 'white-web-sdk'
 import { DownloadContainer } from './download'
 import { StorageContainer } from './storage'
 import { UploadContainer } from './upload'
+import { mapFileType } from '../../../services/upload-service'
+import { formatFileSize } from 'agora-scenario-ui-kit'
+import {BehaviorSubject} from 'rxjs'
 
 const calcUploadFilesMd5 = async (file: File) => {
   return new Promise(resolve => {
-    const time = new Date().getTime(); 
+    const time = new Date().getTime();
     const fileReader = new FileReader();
     fileReader.readAsArrayBuffer(file); //计算文件md5
-    fileReader.onload = async() => {
-        const md5Str = MD5(fileReader.result); 
-        const et= new Date().getTime();
-        resolve(md5Str);
+    fileReader.onload = async () => {
+      const md5Str = MD5(fileReader.result);
+      const et = new Date().getTime();
+      resolve(md5Str);
     };
   });
+}
+
+interface uploadFileInfoProps {
+  iconType: any,
+  fileName: string,
+  fileSize: string,
+  uploadComplete: boolean,
 }
 
 export const CloudDriverContainer = observer((props: any) => {
 
   const boardStore = useBoardStore()
+
+  const checkList$ = new BehaviorSubject<string[]>([])
+
+  const [checkedList, updateList] = useState<string[]>([])
+
+  const fileRef = useRef<HTMLInputElement | null>(null)
+
+  useEffect(() => {
+    checkList$.subscribe({
+      next: (ids: string[]) => {
+        updateList(ids)
+      }
+    })
+    return () => {
+      checkList$.unsubscribe()
+    }
+  }, [])
+
+  const captureCheckedItems = (items: string[]) => {
+    checkList$.next(items)
+  }
+
+  const [showUploadModal, setShowUploadModal] = useState<boolean>(false)
+  const [showUploadToast, setShowUploadToast] = useState<boolean>(false)
+  const [uploadFileInfo, setUploadFileInfo] = useState<uploadFileInfoProps>({
+    iconType: '',
+    fileName: '',
+    fileSize: '',
+    uploadComplete: false,
+  })
+  const [currentProgress, setCurrentProgress] = useState<number>(0)
 
   const onCancel = () => {
     boardStore.setTool('')
@@ -35,7 +76,14 @@ export const CloudDriverContainer = observer((props: any) => {
 
   const handleChange = (key: string) => {
     setActiveKey(key)
+  }
 
+  const showToastFn = () => {
+    setShowUploadModal(false)
+    setShowUploadToast(true)
+    setTimeout(() => {
+      setShowUploadToast(false)
+    }, 1000)
   }
 
   const handleUpload = async (evt: any) => {
@@ -46,7 +94,7 @@ export const CloudDriverContainer = observer((props: any) => {
     const name = file.name.split(".")[0]
     const ext = file.name.split(".").pop()
     // hideToast()
-    const supportedFileTypes = ['bmp','jpg','png','gif','pdf','pptx','mp3','mp4','doc','docx']
+    const supportedFileTypes = ['bmp', 'jpg', 'png', 'gif', 'pdf', 'pptx', 'mp3', 'mp4', 'doc', 'docx']
 
     const needConvertingFile = ['ppt', 'pptx', 'doc', 'docx', 'pdf']
     const isNeedConverting = needConvertingFile.includes(ext)
@@ -60,31 +108,62 @@ export const CloudDriverContainer = observer((props: any) => {
       resourceUuid: resourceUuid,
       converting: isNeedConverting,
       kind: isDynamic ? PPTKind.Dynamic : PPTKind.Static,
-      onProgress: async(evt: any) => {
-        // const { progress, isTransFile  = false , isLastProgress = false } = evt;
-        // const parent = Math.floor(progress * 100)
-        // // setProcess(parent)
-        // // setIsTrans(isTransFile)
-        // // if (isLastProgress && parent == 100) {
-        // //   prepareToast('success', t('disk.uploadSuccess'))
-        // //   setTimeout(() => {
-        // //     showToast(() => {setIsUploadFile(false)})
-        // //   }, 1000)
-        // // }
-        // // if (cancelFileList.includes(resourceUuid) && progress === 100) {
-        // //   await boardStore.removeMaterialList([file.fileID])
-        // // }
+      onProgress: async (evt: any) => {
+        const { progress, isTransFile = false, isLastProgress = false } = evt;
+        const parent = Math.floor(progress * 100)
+        setCurrentProgress(parent)
+        if (isLastProgress && parent == 100) {
+          if (isTransFile) {
+            // 转换文件，需要转化成功后show toast
+            setUploadFileInfo({
+              ...uploadFileInfo,
+              uploadComplete: true,
+            })
+            if (progress === 1) {
+              showToastFn()
+            }
+          } else {
+            // 非转换文件，直接show toast
+            showToastFn();
+          }
+        }
+
+        // if (isLastProgress && parent == 100) {
+        //   prepareToast('success', t('disk.uploadSuccess'))
+        //   setTimeout(() => {
+        //     showToast(() => {setIsUploadFile(false)})
+        //   }, 1000)
+        // }
+        // if (cancelFileList.includes(resourceUuid) && progress === 100) {
+        //   await boardStore.removeMaterialList([file.fileID])
+        // }
       },
       pptConverter: boardStore.boardClient.client.pptConverter(boardStore.room.roomToken)
     }
     if (ext === 'pptx') {
       EduLogger.info("upload dynamic pptx")
     }
-    await boardStore.handleUpload(payload)
+    // TODO: 渲染UI
+    setUploadFileInfo({
+      ...uploadFileInfo,
+      iconType: 'format-' + mapFileType(ext),
+      fileName: name,
+      fileSize: formatFileSize(file.size),
+      uploadComplete: false,
+    })
+    setShowUploadModal(true)
+    try {
+      await boardStore.handleUpload(payload)
+      fileRef.current!.value = ""
+    } catch (e) {
+      fileRef.current!.value = ""
+      throw e
+    }
+
   }
 
-  const handleDelete = () => {
-
+  const handleDelete = async () => {
+    await boardStore.removeMaterialList(checkList$.getValue())
   }
 
   console.log(' driver props ', props)
@@ -94,8 +173,6 @@ export const CloudDriverContainer = observer((props: any) => {
       boardStore.refreshState()
     }
   }, [activeKey, boardStore])
-
-  const fileRef = useRef<HTMLInputElement | null>(null)
 
   const triggerUpload = () => {
     if (fileRef.current) {
@@ -107,7 +184,7 @@ export const CloudDriverContainer = observer((props: any) => {
   return (
     <div className="agora-board-resources">
       <div className="btn-pin">
-        <Icon type="close" style={{cursor: 'pointer'}} hover onClick={onCancel}></Icon>
+        <Icon type="close" style={{ cursor: 'pointer' }} hover onClick={onCancel}></Icon>
       </div>
       <Tabs activeKey={activeKey} onChange={handleChange}>
         <TabPane tab={t('cloud.publicResources')} key="1">
@@ -122,10 +199,37 @@ export const CloudDriverContainer = observer((props: any) => {
               {t('cloud.upload')}
             </Button>
             <Button type="ghost" onClick={handleDelete}>{t('cloud.delete')}</Button>
+            {showUploadToast ? (<Toast style={{ position: 'absolute', left: '50%', top: '50%', transform: 'translate(-50%, -50%)' }}>上传成功</Toast>) : ''}
+            {showUploadModal ? (
+
+              <Modal
+                title="上传"
+                width={450}
+                style={{
+                  position: 'absolute',
+                  top: '50%',
+                  left: '50%',
+                  transform: 'translate(-50%, -50%)'
+                }}
+                showMask={true}
+                onCancel={() => { setShowUploadModal(false) }}
+              >
+                <Loading
+                  hasLoadingGif={false}
+                  uploadItemList={
+                    [
+                      { ...uploadFileInfo, currentProgress }
+                    ]
+                  }
+                />
+              </Modal>
+
+            ) : ""}
+
           </Row>
-          <UploadContainer />
+          <UploadContainer handleUpdateCheckedItems={captureCheckedItems} />
         </TabPane>
-        <TabPane tab={t('cloud.downloadResources')}  key="3">
+        <TabPane tab={t('cloud.downloadResources')} key="3">
           <DownloadContainer />
         </TabPane>
       </Tabs>
--- packages/agora-education-demo/src/pages/common-containers/cloud-driver/upload.tsx
+++ packages/agora-education-demo/src/pages/common-containers/cloud-driver/upload.tsx
@@ -1,17 +1,25 @@
 import { observer } from 'mobx-react'
-import { Button, Col, IconBox, Progress, Inline, Row, CheckBox, Table, TableHeader, formatFileSize, t } from 'agora-scenario-ui-kit'
-import React, {useCallback, useMemo, useState} from 'react'
+import { Button, Col, IconBox, Progress, Inline, Row, CheckBox, Table, TableHeader, formatFileSize, t, Placeholder } from 'agora-scenario-ui-kit'
+import React, {useCallback, useEffect, useMemo, useState} from 'react'
 import { useBoardStore } from '@/hooks'
 import { agoraCaches } from '@/utils/web-download.file'
 import dayjs from 'dayjs'
 
-export const UploadContainer = observer(() => {
+export interface UploadContainerProps {
+  handleUpdateCheckedItems: (ids: string[]) => void
+}
+
+export const UploadContainer: React.FC<UploadContainerProps> = observer(({handleUpdateCheckedItems}) => {
 
   // const {t} = useI18nContext()
 
   const boardStore = useBoardStore()
 
   const [checkMap, setCheckMap] = useState<Record<string, any>>({})
+
+  useEffect(() => {
+    handleUpdateCheckedItems(Object.keys(checkMap))
+  }, [checkMap])
 
   const items = useMemo(() => {
     return boardStore.personalResources.map((it: any) => ({
@@ -69,7 +77,7 @@ export const UploadContainer = observer(() => {
         <Col>{t('cloud.updatedAt')}</Col>
       </TableHeader>
       <Table className="table-container">
-        {items.map(({ id, name, size, updateTime, type, checked }: any, idx: number) =>
+        {items.length ? items.map(({ id, name, size, updateTime, type, checked }: any, idx: number) =>
           <Row height={10} border={1} key={idx}>
             <Col width={9}>
               <CheckBox className="checkbox" onClick={(evt: any) => {
@@ -87,7 +95,7 @@ export const UploadContainer = observer(() => {
               <Inline color="#586376">{dayjs(updateTime).format("YYYY-MM-DD HH:mm:ss")}</Inline>
             </Col>
           </Row>
-        )}
+        ) : <Placeholder placeholderType="noFile"/>}
       </Table>
     </Table>
   )
