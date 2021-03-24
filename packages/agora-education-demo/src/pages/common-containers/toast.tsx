0f8df32199c505484cd94c2068aafae79a55e3b9
Qianze Zhang <qianze.zhang@hotmail.com>
2021-03-25 01:17:31 +0800

    toast support

--- packages/agora-education-demo/src/pages/common-containers/toast.tsx
+++ packages/agora-education-demo/src/pages/common-containers/toast.tsx
@@ -1,8 +1,23 @@
 import { observer } from 'mobx-react'
 import React from 'react'
+import { Toast } from 'agora-scenario-ui-kit'
+import { useUIStore } from '@/hooks'
+import { ToastType } from '@/stores/app/ui'
 
 export const ToastContainer = observer(() => {
+  const uiStore = useUIStore()
   return (
-    <div></div>
+    <div style={{justifyContent: 'center', display: 'flex'}}>
+      {uiStore.toastQueue.map((value: ToastType, idx: number) => 
+        <Toast
+          style={{position:'absolute', top: 50}}
+          key={`${value.id}`}
+          type={value.type}
+          closeToast={() => {
+            uiStore.removeToast(`${value.id}`)
+          }}
+        >{value.desc}</Toast>
+      )}
+    </div>
   )
 })
