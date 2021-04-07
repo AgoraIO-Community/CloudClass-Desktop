b9e9876bf075ddf9bd9a0c3a3bad915990c2d863
matrixbirds <yooobuntu@gmail.com>
2021-04-07 23:39:05 +0800

    fix. freeze

--- packages/agora-rte-demo/src/stores/app/scene.ts
+++ packages/agora-rte-demo/src/stores/app/scene.ts
@@ -1126,12 +1126,10 @@ export class SceneStore extends SimpleInterval {
 
   queryVideoFrameIsNotFrozen (uid: number): boolean {
     const isLocal = +get(this, 'cameraEduStream.streamUuid', 0) === +uid
-    if (this.appStore.mediaStore.localVideoState === LocalVideoStreamState.LOCAL_VIDEO_STREAM_STATE_FAILED) {
-      return false
-    }
     if (isLocal) {
-      // const frameRate = this.appStore.mediaStore.rendererOutputFrameRate[`${0}`]
-      // return frameRate > 0
+      if (this.appStore.mediaStore.localVideoState === LocalVideoStreamState.LOCAL_VIDEO_STREAM_STATE_FAILED) {
+        return false
+      }
       const freezeCount = this.cameraRenderer?.freezeCount || 0
       return freezeCount < 3
     } else {
