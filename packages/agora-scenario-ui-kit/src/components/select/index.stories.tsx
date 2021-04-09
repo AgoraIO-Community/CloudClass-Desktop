2cdd4c23f4ab343d3770fb2dcb733c0dff0cd281
qianfengg <fred_gqf0221@163.com>
2021-04-09 17:17:55 +0800

    feat: add props onSelect

--- packages/agora-scenario-ui-kit/src/components/select/index.stories.tsx
+++ packages/agora-scenario-ui-kit/src/components/select/index.stories.tsx
@@ -8,9 +8,11 @@ const meta: Meta = {
     component: Select,
 }
 
-const optionData = [...'.'.repeat(10)].map((item, index) => ({
-    value: index + "test".repeat(100)
-}))
+const optionData = [...'.'.repeat(5)].map((item, index) => ({
+    value: index
+})).concat([...'.'.repeat(5)].map((item, index) => ({
+    value: index
+})))
 
 export const Docs = () => (
     <>
@@ -18,6 +20,9 @@ export const Docs = () => (
             <Select
                 placeholder={'请选择数字'}
                 // defaultValue={'1'}
+                onSelect={value => {
+                    console.log(value)
+                }}
             >
                 {optionData.map((item, index) => (<Option value={item.value} key={index}>{item.value}</Option>))}
             </Select>
