e2e95c6b1d735783e06b406922f5c1d5336bfa0d
matrixbirds <yooobuntu@gmail.com>
2021-05-07 11:11:32 +0800

    bump. electron

---
+++ packages/agora-electron-edu-demo/webpack.renderer.additions.js
@@ -0,0 +1,14 @@
+module.exports = {
+  // module: {
+  //   rules: [
+  //     {
+  //       test: /\.js$/,
+  //       loader: 'babel-loader',
+  //       exclude: /node_modules/,
+  //       options: {
+  //         compact: false
+  //       }
+  //     }
+  //   ]
+  // }
+}
