c6b799bb88c26132e168523bbe9fb50579b3dd34
lzg <lizg@easemob.com>
2021-07-02 15:41:36 +0800

    fix:本地搜索

--- packages/agora-chat-widget/src/components/MessageBox/UserList/SearchList.js
+++ packages/agora-chat-widget/src/components/MessageBox/UserList/SearchList.js
@@ -1,5 +1,5 @@
 import { useSelector } from 'react-redux'
-import { useState,useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import { Flex, Text, Image } from 'rebass'
 import { Switch, Tag } from 'antd';
 import _ from 'lodash'
@@ -16,6 +16,7 @@ const SearchList = ({ roomListInfo, searchUser, onSetMute, muteMembers }) => {
         setDataLength(false)
     }, [searchUser])
     const roomOwner = useSelector((state) => state.room.info.owner);
+    const roomUsers = useSelector(state => state.room.users)
     // const roomAdmins = useSelector((state) => state.room.admins);
     // 新的空数组，存用户头像，昵称，环信ID
     const aryList = [];
@@ -23,8 +24,9 @@ const SearchList = ({ roomListInfo, searchUser, onSetMute, muteMembers }) => {
         const userName = roomListInfo[item].nickname;
         const avatarUrl = roomListInfo[item].avatarurl;
         const roleType = roomListInfo[item].ext;
-        aryList.push([userName, avatarUrl, item, roleType]);
-        return aryList;
+        if (roomUsers.includes(item)) {
+            aryList.push([userName, avatarUrl, item, roleType]);
+        }
     })
 
     let speakerTeacher = []
@@ -55,7 +57,7 @@ const SearchList = ({ roomListInfo, searchUser, onSetMute, muteMembers }) => {
                 break;
         }
     })
-    const roomUserList = _.concat(speakerTeacher, coachTeacher,audience, student)
+    const roomUserList = _.concat(speakerTeacher, coachTeacher, audience, student)
     return (
         <div>
             {
@@ -64,7 +66,7 @@ const SearchList = ({ roomListInfo, searchUser, onSetMute, muteMembers }) => {
                         return null
                     } else {
                         if (member[0] && (member[0].toLocaleLowerCase()).indexOf(searchUser.toLocaleLowerCase()) !== -1) {
-                            if(!dataLength){
+                            if (!dataLength) {
                                 setDataLength(true)
                             }
                             return (
@@ -74,8 +76,8 @@ const SearchList = ({ roomListInfo, searchUser, onSetMute, muteMembers }) => {
                                             <Image src={member[1] || avatarUrl} className='list-user-img' />
                                             {Number(member[3]) === 2 && muteMembers.includes(member[2]) && <Image className='list-user-forbid' src={forbid} />}
                                         </div>
-                                        
-                                        
+
+
                                         <Flex ml='8px'>
                                             {Number(member[3]) === 1 && <Tag className='tags' ><Text className='tags-txt' ml='4px' mt='1px'>主讲老师</Text></Tag>}
                                             {Number(member[3]) === 3 && <Tag className='tags' ><Text className='tags-txt' ml='4px' mt='1px'>辅导老师</Text></Tag>}
@@ -91,13 +93,13 @@ const SearchList = ({ roomListInfo, searchUser, onSetMute, muteMembers }) => {
                                         />
                                     } */}
                                     {
-                                        Number(member[3]) === 2 && 
+                                        Number(member[3]) === 2 &&
                                         <RcTooltip placement="top" overlay={muteMembers.includes(member[2]) ? '解除禁言' : '禁言'} >
                                             <div className='voice-img-box'>
-                                                <img 
-                                                className='voice-img'
-                                                src={muteMembers.includes(member[2])? voiceNo : voiceOff}
-                                                onClick={onSetMute(member[2])}/>
+                                                <img
+                                                    className='voice-img'
+                                                    src={muteMembers.includes(member[2]) ? voiceNo : voiceOff}
+                                                    onClick={onSetMute(member[2])} />
                                             </div>
                                         </RcTooltip>
                                     }
@@ -107,7 +109,7 @@ const SearchList = ({ roomListInfo, searchUser, onSetMute, muteMembers }) => {
                     }
                 })
             }
-            {!dataLength && <div className='no-search'>无搜索结果</div>} 
+            {!dataLength && <div className='no-search'>无搜索结果</div>}
         </div>
     )
 }
