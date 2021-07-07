94dbdcdd21e7868e429b992880d00d79d3970f16
matrixbirds <yooobuntu@163.com>
2021-07-07 10:52:19 +0800

    fix: [RTE] room-chat-message

--- packages/agora-edu-core/src/stores/index.ts
+++ packages/agora-edu-core/src/stores/index.ts
@@ -227,11 +227,10 @@ export class EduScenarioAppStore {
     if (isEmpty(roomInfoParams)) {
       this.load()
       autorun(() => {
-        const data = toJS(this)
+        const roomInfo = toJS(this.roomInfo)
         GlobalStorage.save('agora_edu_room', {
-          roomInfo: data.roomInfo,
+          roomInfo
         })
-        this
       })
     } else {
       this.setRoomInfo({
@@ -242,9 +241,9 @@ export class EduScenarioAppStore {
     }
 
     autorun(() => {
-      const data = toJS(this)
+      const deviceInfo = toJS(this.deviceInfo)
       GlobalStorage.save('agora_edu_device', {
-        deviceInfo: data.deviceInfo
+        deviceInfo
       })
     })
 
