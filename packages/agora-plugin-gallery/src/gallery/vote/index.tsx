e882a8e807adc12cff5e2abd10ef464927acfecf
Qianze Zhang <zhangqianze@agora.io>
2021-08-08 22:40:21 +0800

    APAAS-756

---
+++ packages/agora-plugin-gallery/src/gallery/vote/index.tsx
@@ -0,0 +1,180 @@
+import 'promise-polyfill/src/polyfill';
+import React, { useEffect, useState, useMemo } from 'react';
+import ReactDOM from 'react-dom';
+import { PluginStore, c2h } from './store'
+import { usePluginStore } from './hooks'
+import { Provider, observer } from 'mobx-react';
+import type { IAgoraExtApp, AgoraExtAppContext, AgoraExtAppHandle } from 'agora-edu-core'
+import {
+  Button,
+  Table,
+  TableHeader,
+  Col,
+  Row,
+  Input,
+  transI18n,
+  I18nProvider,
+  changeLanguage
+} from '~ui-kit'
+import classnames from 'classnames'
+import { EduRoleTypeEnum } from 'agora-rte-sdk';
+import "./index.css"
+import reduceSvg from './reduce.svg';
+import addSvg from './add.svg';
+import { runInAction } from 'mobx';
+// import { I18nProvider, transI18n, changeLanguage } from '../../gallery-ui-kit/components/i18n'
+
+const App = observer(({ onHeight, onTitle, lang }: { onHeight: (height: number) => void, onTitle: (title: string) => void, lang:'zh'|'en' }) => {
+  const pluginStore = usePluginStore()
+
+  useEffect(() => {
+    onHeight(pluginStore.height || 0)
+  }, [pluginStore.height])
+
+  const globalEvents = pluginStore.context.contexts.global.events
+  const userlistEvents = pluginStore.context.contexts.userList.events
+
+  useEffect(() => {
+    globalEvents.subscribe((state:any) => {
+      // pluginStore.updateGlobalContext(state)
+    })
+    userlistEvents.subscribe((state:any) => {
+      runInAction(() => {
+        pluginStore.userList = state.userList
+        pluginStore.rosterUserList = state.rosterUserList
+      })
+    })
+  }, [])
+
+  return (
+    <div
+      style={{
+        width: '100%',
+        height: '100%'
+      }}
+      className={classnames({
+        [`vote-modal`]: 1,
+        [`vote-language-en`]: lang === 'en'
+      })}
+    >
+      {pluginStore.status !== 'config' && pluginStore.context.localUserInfo.roleType !== EduRoleTypeEnum.teacher ? <span className='vote-title-tip'>{transI18n(pluginStore.mulChoice ? 'vote.mul-sel' : 'vote.single-sel')}</span> : null}
+      <div>
+        <textarea
+          value={pluginStore.title}
+          className="vote-title"
+          placeholder={transI18n('vote.input-tip')}
+          disabled={pluginStore.status !== 'config'}
+          style={pluginStore.status !== 'config' ? { height: c2h(pluginStore.title?.length || 0) + 'px' } : undefined}
+          maxLength={100}
+          onChange={(e: any) => { pluginStore.status === 'config' && (pluginStore.title = e.target.value) }}
+        />
+        {pluginStore.status === 'config' ? <>
+          <span className='vote-limit' >{pluginStore.title?.length || 0}/100</span>
+          <div className="vote-mulchoice" >
+            <label ><input type="radio" name='sel' checked={!pluginStore.mulChoice} onChange={() => { }} onClick={() => { pluginStore.status === 'config' && (pluginStore.mulChoice = false) }} /><span></span>{transI18n('vote.single-sel')}</label>
+            <label ><input type="radio" name='sel' checked={pluginStore.mulChoice} onChange={() => { }} onClick={() => { pluginStore.status === 'config' && (pluginStore.mulChoice = true) }} /><span></span>{transI18n('vote.mul-sel')}</label>
+          </div>
+        </> : null}
+        <Table>
+          {pluginStore.answer?.map((col: string, idx: number) => (
+            <Row key={idx} className={pluginStore.status !== 'config' && pluginStore.status !== 'answer'?'vote-process-show':''} style={{ padding: pluginStore.status==='config'?'0':'0 20px 0 0' }}>
+              {pluginStore.status === 'end' || pluginStore.status === 'info' ? <span
+                style={{
+                  color: '#677386',
+                  fontSize: '14px',
+                  position: 'relative',
+                  top: '9px',
+                  display: 'inline-block',
+                  width: '30px',
+                  textAlign: 'center'
+                }}>{idx + 1}.</span> : (pluginStore.status === 'answer' ?
+                <label className="vote-item-prefix">
+                  <input
+                    type="radio"
+                    checked={pluginStore.selAnswer?.includes(col)} onChange={() => { }}
+                    onClick={() => { pluginStore.changeSelAnswer(col, pluginStore.mulChoice) }}
+                  />
+                  <span className={pluginStore.mulChoice?'vote-item-mul':''} ></span>
+                </label>
+                : null)}
+              <Input
+                className="vote-item"
+                prefix={pluginStore.status === 'config' ? <span
+                    style={{
+                      color: '#677386',
+                      fontSize: '14px'
+                    }}>{idx + 1}.</span> : null}
+                suffix={pluginStore.status === 'end' || pluginStore.status === 'info' ? <span style={{
+                  color: '#677386',
+                  fontSize: '14px'
+                }}>{pluginStore.answerInfo ? pluginStore.answerInfo[idx] : ''}</span> : null}
+                value={pluginStore.answer && pluginStore.answer[idx]}
+                disabled={pluginStore.status !== 'config'}
+                placeholder={transI18n('vote.item-tip')}
+                onChange={(e: any) => { pluginStore.changeAnswer(idx, e.target.value) }}>
+              </Input>
+              {pluginStore.status === 'info' || pluginStore.status === 'end' ? <span className="vote-process" style={{ width: 'calc(100% - 50px)' }}><span
+                style={{ width: pluginStore.answerInfo ? pluginStore.answerInfo[idx].split(' ')[1] : '0' }} ></span></span> : null}
+            </Row>
+          ))}
+        </Table>
+      </div>
+      {pluginStore.ui?.includes('subs') ? <div className={pluginStore.status === 'config' ? "vote-submit" : "vote-submit vote-vote"}>
+        <span onClick={() => {
+          pluginStore.addAnswer()
+        }} style={{ backgroundImage: `url(${addSvg})`, visibility: (pluginStore.status !== 'config') || ((pluginStore.answer?.length || 4) > 4) ? 'hidden' : 'visible' }} ></span>
+        <Button
+          disabled={pluginStore.status === 'config' ? (!pluginStore.title || (pluginStore.answer?.includes('') || false))
+            : (pluginStore.status === 'answer' ? (pluginStore.selAnswer?.length || 0) === 0 : false)}
+          onClick={() => {
+            pluginStore.onSubClick();
+          }}
+        >{transI18n(pluginStore?.buttonName || '')}</Button>
+        <span onClick={() => {
+          pluginStore.subAnswer()
+        }} style={{ backgroundImage: `url(${reduceSvg})`, visibility: (pluginStore.status !== 'config') || ((pluginStore.answer?.length || 4) < 3) ? 'hidden' : 'visible' }}></span>
+      </div> : null}
+    </div>
+  )
+})
+
+
+export class AgoraExtAppVote implements IAgoraExtApp {
+  appIdentifier = "io.agora.vote"
+  appName = 'vote'
+  className = 'vote-dialog'
+  width = 360
+  height = 283
+  title = transI18n('vote.appName')
+
+  store?: PluginStore
+
+  constructor(public readonly language: any = 'en') {
+    changeLanguage(this.language)
+  }
+
+  extAppDidLoad(dom: Element, ctx: AgoraExtAppContext, handle: AgoraExtAppHandle): void {
+    this.store = new PluginStore(ctx, handle)
+    ReactDOM.render((
+      <I18nProvider language={this.language}>
+        <Provider store={this.store}>
+          <App onHeight={(height: number) => {
+            this.height = height
+          }} onTitle={(title: string) => {
+            this.title = title
+          }} lang={this.language} />
+        </Provider>
+      </I18nProvider>
+    ),
+      dom
+    );
+  }
+  extAppRoomPropertiesDidUpdate(properties: any, cause: any): void {
+    this.store?.onReceivedProps(properties, cause)
+  }
+  extAppWillUnload(): void {
+    if (this.store?.context.localUserInfo.roleType === EduRoleTypeEnum.teacher) {
+      this.store?.onSubClick(true)
+    }
+  }
+}
---
+++ packages/agora-plugin-gallery/src/gallery/vote/store.ts
@@ -0,0 +1,351 @@
+import { action, observable, computed, runInAction } from 'mobx';
+import type { AgoraExtAppContext, AgoraExtAppHandle } from 'agora-edu-core'
+import { EduRoleTypeEnum } from 'agora-rte-sdk';
+import { Toast, transI18n } from '~ui-kit'
+import { threadId } from 'worker_threads';
+
+const formatTime = (long: number) => {
+    let h: any = Math.floor(long / (60 * 60));
+    h = (h < 10 ? '0' : '') + h
+    let m: any = Math.floor(long / 60) % 60;
+    m = (m < 10 ? '0' : '') + m
+    let s: any = Math.floor(long % 60);
+    s = (s < 10 ? '0' : '') + s
+    return h + ':' + m + ':' + s;
+}
+
+const getStudentInfo = (info: any) => {
+    if (info === null || typeof info === 'undefined' || info === 'deleted' || JSON.stringify(info) === "{}") {
+        return null;
+    }
+    return info;
+}
+
+export const c2h = (count: number)=>{
+    if (count > 60) {
+      return 120;
+    }else if(count > 40){
+      return 90
+    }
+    return 75
+  }
+
+export class PluginStore {
+    context: AgoraExtAppContext
+    handle: AgoraExtAppHandle
+
+    @observable
+    height?: number = 283
+    @observable
+    title?: string = ''
+    @observable
+    answer?: string[] = ['', '']
+    @observable
+    selAnswer?: string[] = []
+    @observable
+    currentTime?: string = ''
+    @observable
+    students?: any[]
+    @observable
+    status: string = 'config'//config,answer,info,end
+    @observable
+    answerInfo?: string[] = []
+    @observable
+    buttonName?: string = 'vote.start'
+    @observable
+    ui?: string[] = ['sels']//'sels','users','infos','subs'
+    @observable
+    timehandle?: any = null
+    @observable
+    mulChoice?: boolean = false
+
+    @observable
+    rosterUserList?: any[]
+    @observable
+    userList?: any[]
+
+    constructor(ctx: AgoraExtAppContext, handle: AgoraExtAppHandle) {
+        this.context = ctx
+        this.handle = handle
+        this.onReceivedProps(ctx.properties || {}, null)
+    }
+
+    @action
+    updateTime = () => {
+        if (this.status === 'config') {
+            this.currentTime = ''
+        } else {
+            let properties = this.context.properties
+            this.currentTime = formatTime(properties.endTime ? Number(properties.endTime) - Number(properties.startTime) : Math.floor(Date.now() / 1000) - Number(properties.startTime))
+        }
+
+        if (this.status === 'config' || this.status === 'end' || this.context.properties.endTime) {
+            this.timehandle && clearInterval(this.timehandle);
+            this.timehandle = null
+        } else {
+            this.timehandle || (this.timehandle = setInterval(() => {
+                this.updateTime()
+            }, 1000))
+        }
+    }
+
+    changeRoomProperties = async ({
+        state,
+        canChange = false,
+        mulChoice,
+        startTime,
+        endTime,
+        title,
+        items,
+        answer,
+        replyTime,
+        commonState
+    }: {
+        state?: string,//config(老师本地配置),start(开始答题/投票),end(答题/投票结束)
+        canChange?: boolean,//是否允许修改(投票为false，答题为true)
+        mulChoice?: boolean,//是否多选(答题为true，投票可设置)
+        startTime?: string,
+        endTime?: string,
+        title?: string,
+        items?: Array<string>,//可选内容
+        answer?: Array<string>,//正确答案(答题用，投票忽略)
+        replyTime?: string,
+        commonState?: number
+    }) => {
+        let roomProperties: any = {}
+        if (this.context.localUserInfo.roleType === EduRoleTypeEnum.teacher) {
+            ['start','end'].includes(state||'') && (roomProperties['state'] = state)
+            canChange && (roomProperties['canChange'] = canChange)
+            typeof mulChoice !== 'undefined' && (roomProperties['mulChoice'] = mulChoice)
+            startTime && (roomProperties['startTime'] = startTime)
+            endTime && (roomProperties['endTime'] = endTime)
+            items && (roomProperties['items'] = items)
+            title && (roomProperties['title'] = title)
+            answer && (roomProperties['answer'] = answer)
+            if (state === "start") {
+                roomProperties['students'] = []
+                roomProperties['studentNames'] = []
+                this.userList?.map((user: any) => {
+                    if (!(/^guest[0-9]{10}2$/).test(user.userUuid || '')) {
+                        roomProperties['students'].push(user.userUuid)
+                        roomProperties['studentNames'].push(user.userName)
+                    }
+                });
+            } else if (state === "updateStudent") {
+                if (this.rosterUserList) {
+                    let users:any = []
+                    this.userList?.map((user: any) => {
+                        if (!(/^guest[0-9]{10}2$/).test(user.userUuid || '')) {
+                            users.push(user.userUuid)
+                        }
+                    });
+
+                    roomProperties['students'] = []
+                    roomProperties['studentNames'] = []
+                    this.rosterUserList?.map((user: any) => {
+                        if (!(/^guest[0-9]{10}2$/).test(user.userUuid || '')) {
+                            roomProperties['students'].push(user.userUuid)
+                            roomProperties['studentNames'].push(user.userName)
+                        }
+                    }); 
+                    if (JSON.stringify(users.slice().sort()) !== JSON.stringify(roomProperties['students'].slice().sort())) {
+                        this.userList = this.rosterUserList
+                        console.log(roomProperties);
+                        
+                    }else{
+                        return;
+                    }
+                }else{
+                    return;
+                }
+            } else if (state === "clear") {
+                roomProperties['students'] = []
+                roomProperties['studentNames'] = []
+                roomProperties['state'] = ''
+                roomProperties['canChange'] = false
+                roomProperties['mulChoice'] = false
+                roomProperties['startTime'] = ''
+                roomProperties['endTime'] = ''
+                roomProperties['title'] = ''
+                roomProperties['items'] = []
+                roomProperties['answer'] = []
+                this.changeRoomProperties({ state: 'clearStudent' })
+            } else if (state === "clearStudent") {
+                let propers: string[] = [];
+                let keys = Object.keys(this.context.properties)
+                keys.map((ele: string) => {
+                    ['answer','canChange','title','endTime','items','mulChoice','startTime','state','studentNames','students'].includes(ele) || propers.push(ele)
+                })
+                console.log('clearStudent:',this.context.properties.students,propers,this.context.properties);
+                await this.handle.deleteRoomProperties(propers, {})
+                return;
+            }
+            await this.handle.updateRoomProperty(roomProperties, { state: commonState || 0 }, {})
+        } else {
+            if (this.context.properties.state === "start") {
+                roomProperties['student' + this.context.localUserInfo.userUuid] = { replyTime, answer }
+                await this.handle.updateRoomProperty(roomProperties, { state: commonState || 0 }, {});
+            }
+        }
+    }
+
+    onSubClick = (clear: boolean = false) => {
+        if (this.context.localUserInfo.roleType === EduRoleTypeEnum.teacher) {
+            if (clear) {
+                this.changeRoomProperties({ state: 'clear' })
+            } else {
+                if (this.status === 'config') {
+                    let sels: string[] = [];
+                    this.selAnswer?.map((sel: string) => {
+                        if (this.answer?.includes(sel)) {
+                            sels.push(sel)
+                        }
+                    })
+                    //this.changeRoomProperties({ state: 'clearStudent' })//删除属性会引起插件被关闭
+                    this.changeRoomProperties({ state: 'start', startTime: (Math.floor(Date.now() / 1000)).toString(), title: this.title, items: this.answer, mulChoice: this.mulChoice, answer: sels, commonState: 1 })
+                } else if (this.status === 'info') {
+                    this.changeRoomProperties({ state: 'end', endTime: (Math.floor(Date.now() / 1000)).toString(), commonState: 1 })
+                } else {
+                    this.changeRoomProperties({ state: 'clear' })
+                }
+            }
+        } else {
+            if (this.status === 'answer') {
+                this.changeRoomProperties({ replyTime: (Math.floor(Date.now() / 1000)).toString(), answer: this.selAnswer, commonState: 1 })
+            }
+        }
+    }
+
+    @action
+    onReceivedProps(properties: any, cause: any) {
+        this.context.properties = properties
+        if (this.context.localUserInfo.roleType === EduRoleTypeEnum.teacher) {
+            if (properties.state === 'start' || properties.state === 'end') {
+                this.title = properties.title
+                this.answer = properties.items
+                this.mulChoice = properties.mulChoice
+                this.selAnswer = []
+                this.students = []
+                let selNumber = [0,0,0,0,0,0,0,0,0,0];
+                properties.students.map((student: string, index: number) => {
+                    let answer = getStudentInfo(properties['student' + student])
+                    if (answer && answer.answer) {
+                        answer.answer.map((sel:string)=>{
+                            let idx = properties.items.indexOf(sel)
+                            idx >= 0 && selNumber[idx]++
+                        })
+                    }
+                })
+                
+                properties.items.map((item:string,index:number)=>{
+                    this.answerInfo && (this.answerInfo[index] = `(${selNumber[index]}) ${Math.floor(selNumber[index]*100/(properties.students.length||1))}%`)
+                })
+                this.status = properties.state === 'end' ? 'end' : 'info'
+                this.height = c2h(properties.title?.length || 0) + (this.answer?.length||0)*50 - 10 + (properties.state === 'end' ? 0 : 116)
+                this.buttonName = properties.state === 'end' ? 'vote.restart' : 'vote.over'
+                this.ui = properties.state === 'end' ? ['users', 'infos'] : ['users', 'infos', 'subs']
+            } else {
+                this.title = ""
+                this.answer = ['', '']
+                this.selAnswer = []
+                this.currentTime = ''
+                this.students = []
+                this.status = 'config'
+                this.height = 283
+                this.buttonName = 'vote.start'
+                this.ui = ['sels', 'subs']
+            }
+        } else {
+            if (properties.state === 'start' && !getStudentInfo(properties['student' + this.context.localUserInfo.userUuid])) {
+                this.title = properties.title
+                this.answer = properties.items
+                this.mulChoice = properties.mulChoice
+                this.status = 'answer'
+                this.height = c2h(properties.title?.length || 0) + (this.answer?.length||0)*50 - 10 + 116
+                this.buttonName = !getStudentInfo(properties['student' + this.context.localUserInfo.userUuid]) ? 'vote.submit' : 'vote.change'
+                this.ui = ['sels', 'subs']
+            } else if (properties.state === 'end' || properties.state === 'start') {
+                this.title = properties.title
+                this.answer = properties.items
+                this.mulChoice = properties.mulChoice
+                this.selAnswer = getStudentInfo(properties['student' + this.context.localUserInfo.userUuid])?.answer || []
+                this.status = 'info'
+                this.height = c2h(properties.title?.length || 0) + (this.answer?.length||0)*50 - 10 + 20
+                this.ui = ['infos']
+
+                let selNumber = [0,0,0,0,0,0,0,0,0,0];
+                properties.students.map((student: string, index: number) => {
+                    let answer = getStudentInfo(properties['student' + student])
+                    if (answer && answer.answer) {
+                        answer.answer.map((sel:string)=>{
+                            let idx = properties.items.indexOf(sel)
+                            idx >= 0 && selNumber[idx]++
+                        })
+                    }
+                })
+                
+                properties.items.map((item:string,index:number)=>{
+                    this.answerInfo && (this.answerInfo[index] = `(${selNumber[index]}) ${Math.floor(selNumber[index]*100/(properties.students.length||1))}%`)
+                })
+            } else {
+                this.status = 'config'
+                this.title = ""
+                this.answer = ['', '']
+                this.selAnswer = []
+                this.currentTime = ''
+                this.students = []
+                this.height = 270
+                this.buttonName = 'vote.submit'
+                this.ui = ['infos']
+            }
+        }
+    }
+
+    @action
+    addAnswer() {
+        if (this.answer && this.answer.length < 8) {
+            this.answer.push('')
+            this.height = 433 - (5-this.answer.length)*50
+        }
+    }
+
+    @action
+    subAnswer() {
+        if (this.answer && this.answer.length > 2) {
+            this.answer.splice(this.answer.length - 1, 1)
+            this.height = 433 - (5-this.answer.length)*50
+        }
+    }
+
+    @action
+    changeAnswer(idx:number, answer: string) {
+        if(this.status === 'config'){
+            if (this.answer && this.answer.length > idx && idx >= 0) {
+                this.answer[idx] = answer
+             }
+        }
+    }
+
+    @action
+    changeSelAnswer(sel: string, mul: boolean = true) {
+        if (mul) {
+            if (this.selAnswer?.includes(sel)) {
+                this.selAnswer.splice(this.selAnswer.indexOf(sel), 1);
+            } else {
+                this.selAnswer?.push(sel)
+            }
+        }else{
+            this.selAnswer = [sel]
+        }
+    }
+    
+    @action
+    updateGlobalContext(state:any) {
+        if (this.context.localUserInfo.roleType === EduRoleTypeEnum.teacher) {
+            // this.globalContext = state
+            if (this.status !== 'config') {
+                this.changeRoomProperties({ state: 'updateStudent', commonState: 1 })
+            }
+        }
+    }
+}
