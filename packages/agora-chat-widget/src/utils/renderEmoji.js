b838eafa16a70fadb7700369f06b1246637ec79e
lzg <lizg@easemob.com>
2021-09-08 17:53:10 +0800

    feat: replacing picture emoticons

---
+++ packages/agora-chat-widget/src/utils/renderEmoji.js
@@ -0,0 +1,33 @@
+
+
+import emoji from './emoji'
+// 渲染图片表情
+const renderEmoji = (txtMsg, rnTxt) => {
+    let match = null
+    const regex = /(\[.*?\])/g
+    let start = 0
+    let index = 0
+    while ((match = regex.exec(txtMsg))) {
+        index = match.index
+        if (index > start) {
+            rnTxt.push(txtMsg.substring(start, index))
+        }
+        if (match[1] in emoji.map) {
+            const v = emoji.map[match[1]]
+            rnTxt.push(
+                <img
+                    key={WebIM.conn.getUniqueId()}
+                    src={require(`../themes/faces/${v}`).default}
+                    style={{ width: '20px', height: '20px', display: 'inline-block' }}
+                />
+            )
+        } else {
+            rnTxt.push(match[1])
+        }
+        start = index + match[1].length
+    }
+    rnTxt.push(txtMsg.substring(start, txtMsg.length))
+    return rnTxt;
+}
+
+export default renderEmoji;
