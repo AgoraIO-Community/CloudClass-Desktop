e277cdcec8b57055368521346b52afb06701e185
任一飞 <renyifei@agora.io>
2021-12-10 13:18:00 +0000

    Merge pull request #1085 in ADUC/cloudclass-desktop from chore/apaas-construct to flex
    Squashed commit of the following:
    
    commit 84fa8d21f7c2fafb0b9386d2d76269a856ee61af
    Author: qinzhen <qz519189636@qq.com>
    Date:   Fri Dec 10 16:32:51 2021 +0800
    
        fix:  eslint version
    
    commit 088f5f2ca9bab693d9f42d005cf39f58905f104a
    Author: qinzhen <qz519189636@qq.com>
    Date:   Thu Dec 9 14:52:32 2021 +0800
    
        chore:  eslint-webpack-plugin
    
    commit c8304e855f4fb6312b62c50f41ca0f703ae3207b
    Author: qinzhen <qz519189636@qq.com>
    Date:   Thu Dec 9 14:49:01 2021 +0800
    
        chore:  dependencies
    
    commit 4c8b3dab25d7009f96621dd294ab9c3d02aa3044
    Author: qinzhen <qz519189636@qq.com>
    Date:   Thu Dec 9 14:42:57 2021 +0800
    
        fix: eslint
    
    commit 911de13d5b2eff52930120ecd0e2e84578e40d0b
    Merge: b38bd6c6d a3cbdb783
    Author: qinzhen <qz519189636@qq.com>
    Date:   Thu Dec 9 11:08:52 2021 +0800
    
        fix:  conflict
    
    commit b38bd6c6d0364353753c4f375889ccec63e86d60
    Author: qinzhen <qz519189636@qq.com>
    Date:   Tue Dec 7 16:17:27 2021 +0800
    
        chore:  bundle npm
    
    commit 32bc3421174ec511673a0bff61fe6a2e71141963
    Author: qinzhen <qz519189636@qq.com>
    Date:   Tue Dec 7 15:02:50 2021 +0800
    
        chore:  ui del loading example
    
    commit a1e214ec5981d0682629b9fc272de418f3e6d6a6
    Author: qinzhen <qz519189636@qq.com>
    Date:   Tue Dec 7 14:36:17 2021 +0800
    
        fix:  VideoPreviewPlayer

---
+++ packages/agora-classroom-sdk/.eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+  extends: [
+    'prettier',
+    'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react
+    'plugin:@typescript-eslint/recommended', // Uses the recommended rules from @typescript-eslint/eslint-plugin
+  ],
+  parserOptions: {
+    ecmaVersion: 6, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
+    ecmaFeatures: {
+      jsx: true, // Allows for the parsing of JSX
+    },
+  },
+  rules: {
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
+    },
+  },
+  ignorePatterns: ['webpack/**/*.js', 'app/**/*.js'],
+};
