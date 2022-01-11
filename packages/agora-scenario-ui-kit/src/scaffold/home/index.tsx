6d70c631c46e5cb30adc52d09102493dc8c7eac9
zhangqianze <zhangqianze@agora.io>
2022-01-11 13:41:48 +0000

    Merge pull request #1256 in ADUC/cloudclass-desktop from jira/APAAS-1678 to flex
    Squashed commit of the following:
    
    commit 0443a8782cd508de71bc97bef25939d16f27bd0f
    Author: renyifei <renyifei@agora.io>
    Date:   Tue Jan 11 20:03:14 2022 +0800
    
        fix: [APAAS-1678] len of input

--- packages/agora-scenario-ui-kit/src/scaffold/home/index.tsx
+++ packages/agora-scenario-ui-kit/src/scaffold/home/index.tsx
@@ -276,9 +276,9 @@ export const Home: React.FC<HomeProps> = ({
                 value={roomName}
                 onChange={(evt) => onChangeRoomName(evt.currentTarget.value)}
                 placeholder={transI18n('home.roomName_placeholder')}
-                rule={/^[a-zA-Z0-9]{6,50}$/}
+                rule={/^[a-zA-Z0-9]{6,20}$/}
                 errorMsg={transI18n('home.input-error-msg')}
-                maxLength={50}
+                maxLength={20}
               />
             </Col>
           </Row>
@@ -297,9 +297,9 @@ export const Home: React.FC<HomeProps> = ({
                 value={userName}
                 onChange={(evt) => onChangeUserName(evt.currentTarget.value)}
                 placeholder={transI18n('home.nickName_placeholder')}
-                rule={/^[a-zA-Z0-9]{6,50}$/}
+                rule={/^[a-zA-Z0-9]{6,20}$/}
                 errorMsg={transI18n('home.input-error-msg')}
-                maxLength={50}
+                maxLength={20}
               />
             </Col>
           </Row>
@@ -453,8 +453,8 @@ export const Home: React.FC<HomeProps> = ({
                 !!roomName &&
                 !!role &&
                 !!scenario &&
-                /^[a-zA-Z0-9]{6,50}$/.test(roomName) &&
-                /^[a-zA-Z0-9]{6,50}$/.test(userName)
+                /^[a-zA-Z0-9]{6,20}$/.test(roomName) &&
+                /^[a-zA-Z0-9]{6,20}$/.test(userName)
               )
             }>
             {transI18n('home.enter_classroom')}
