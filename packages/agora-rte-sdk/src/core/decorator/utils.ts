5cbbf8a9826a423aabc6886779e19e926e4f6b4c
renyifei <renyifei@agora.io>
2022-01-19 22:54:31 +0800

    chore: remove files

--- packages/agora-rte-sdk/src/core/decorator/utils.ts
+++
@@ -1,9 +0,0 @@
-export const addIgnoredMethodsToPrototype = (prototype: any, ignoredMethods: string[]) => {
-  let methods: string[] = prototype.__$$ignoreMethods || [];
-  methods = methods.concat(ignoredMethods);
-  prototype.__$$ignoreMethods = methods;
-};
-
-export const isIgnoredMethod = (prototype: any, method: string) => {
-  return prototype.__$$ignoreMethods && prototype.__$$ignoreMethods.indexOf(method) > -1;
-};
