1fb261729d3df7e28095d4d247b62f20481977de
renyifei <renyifei@agora.io>
2022-01-20 20:49:14 +0800

    chore: remove files

--- packages/agora-edu-core/src/stores/ui/common/layout/helper.ts
+++
@@ -1,7 +0,0 @@
-export const getRootDimensions = (containerNode: Window | HTMLElement) => {
-  const height =
-    containerNode instanceof Window ? containerNode.innerHeight : containerNode.clientHeight;
-  const width =
-    containerNode instanceof Window ? containerNode.innerWidth : containerNode.clientWidth;
-  return { width, height };
-};
