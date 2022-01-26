77a537513b625835e3e19a89343f0296848341cf
lizg <903504686@qq.com>
2022-01-26 12:05:28 +0800

    feat: add picture message and screenshots message

---
+++ packages/agora-chat-widget/src/components/InputBox/Screenshot.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import { Menu } from 'antd';
+import { sendImgMsg } from '../../api/message';
+
+const ScreenshotMenu = ({ couterRef }) => {
+  const ipcRenderer = window.require('electron').ipcRenderer;
+  const [isCloaking, setIsCloaking] = useState(true);
+  const isElectron = window.navigator.userAgent.indexOf('Electron') !== -1;
+  const captureScreen = (hideWindow) => {
+    setIsCloaking(!hideWindow);
+    isElectron && ipcRenderer.send('shortcutcapture', { hideWindow });
+  };
+
+  if (isElectron) {
+    const dataURLtoBlob = (dataurl) => {
+      var arr = dataurl.split(','),
+        mime = arr[0].match(/:(.*?);/)[1],
+        bstr = atob(arr[1]),
+        n = bstr.length,
+        u8arr = new Uint8Array(n);
+      while (n--) {
+        u8arr[n] = bstr.charCodeAt(n);
+      }
+      return new Blob([u8arr], { type: mime });
+    };
+
+    ipcRenderer.removeAllListeners('shortcutCaptureDone');
+    ipcRenderer.on('shortcutCaptureDone', (event, dataURL, bounds) => {
+      const blob = dataURLtoBlob(dataURL);
+      //dataURL = window.URL.createObjectURL(blob);
+      let file = {
+        url: dataURL,
+        data: blob,
+        filename: `截图文件${new Date().getTime()}.png`,
+        filetype: 'png',
+        // 该参数用来标识图片发送对象的来源，主要告诉拦截器如果是截图，就不要在输入框生成缩略图了
+        from: 'screenShot',
+        imgsrc: dataURL,
+      };
+      sendImgMsg(couterRef, file);
+    });
+  }
+  return (
+    <Menu defaultSelectedKeys={'1'}>
+      <Menu.Item key="1" className="screenshot-menu" onClick={() => captureScreen(false)}>
+        {isCloaking && <div className="screenshot-menu-select"></div>}
+        <span>截图</span>
+      </Menu.Item>
+      <Menu.Item key="2" className="screenshot-menu" onClick={() => captureScreen(true)}>
+        {!isCloaking && <div className="screenshot-menu-select"></div>}
+        <span>截图时隐藏教室页面</span>
+      </Menu.Item>
+    </Menu>
+  );
+};
+
+export default ScreenshotMenu;
---
+++ packages/agora-chat-widget/src/components/MessageBox/ImgMsg.jsx
@@ -0,0 +1,100 @@
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { Tag, Dropdown, Modal } from 'antd';
+import { ROLE } from '../../contants';
+import { transI18n } from '~ui-kit';
+import './index.css';
+
+export const ImgMsg = ({ item }) => {
+  const state = useSelector((state) => state);
+  const loginUser = state?.loginUser;
+  const roleType = state?.propsData.roleType;
+  const sender = item?.from === loginUser;
+  const teacherTag = item?.ext.role === ROLE.teacher.id;
+  const assistantTag = item?.ext.role === ROLE.assistant.id;
+  const imgUrl = item?.url || item?.body?.url;
+  const useAvatarUrl = item?.ext.avatarUrl;
+  const userNickName = item?.ext.nickName;
+  const isTeacher = roleType === ROLE.teacher.id || roleType === ROLE.assistant.id;
+  const imgHeight = item?.height;
+  const imgWidth = item?.width;
+  const isImgStyle = imgHeight > imgWidth;
+
+  // 控制禁言
+  const [muteVisible, setMuteVisible] = useState(false);
+
+  // 控制大图
+  const [maxImg, setMaxImg] = useState(false);
+  const [maImgUrl, setMaxImgUrl] = useState('');
+
+  // 点击展示大图
+  const showMaximumPicture = (imgUrl) => {
+    setMaxImgUrl(imgUrl);
+    setMaxImg(true);
+  };
+
+  // 点击 禁言/解除禁言 后隐藏
+  const hideMuteVisible = () => {
+    setMuteVisible(!muteVisible);
+  };
+
+  // 监听消息 禁言/解除禁言 变化
+  const handleMuteVisibleChange = (muteVisible) => {
+    setMuteVisible(muteVisible);
+  };
+
+  return (
+    <div>
+      {sender && (
+        <div>
+          <div className="msg-user-me">
+            {teacherTag && <Tag className="msg-tag">{transI18n('chat.teacher')}</Tag>}
+            {assistantTag && <Tag className="msg-tag">{transI18n('chat.assistant')}</Tag>}
+            <span className="msg-from-name">{userNickName}</span>
+            {<img src={useAvatarUrl} className="msg-avatar" alt="" />}
+          </div>
+          <div className="msg-border">
+            <div style={{ margin: '8px' }} onClick={() => showMaximumPicture(imgUrl)}>
+              <img
+                src={imgUrl}
+                style={{
+                  height: isImgStyle ? '160px' : '100px',
+                  width: isImgStyle ? '100px' : '160px',
+                }}
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+      )}
+      {!sender && (
+        <div>
+          <div className="msg-user-other">
+            {<img src={useAvatarUrl} className="msg-avatar" alt="" />}
+            <span className="msg-from-name">{userNickName}</span>
+            {teacherTag && <Tag className="msg-tag">{transI18n('chat.teacher')}</Tag>}
+            {assistantTag && <Tag className="msg-tag">{transI18n('chat.assistant')}</Tag>}
+          </div>
+          <div style={{ margin: '8px' }} onClick={() => showMaximumPicture(imgUrl)}>
+            <img
+              src={imgUrl}
+              style={{
+                height: isImgStyle ? '160px' : '100px',
+                width: isImgStyle ? '100px' : '160px',
+              }}
+              alt=""
+            />
+          </div>
+        </div>
+      )}
+      <Modal
+        onCancel={() => setMaxImg(false)}
+        visible={maxImg}
+        footer={null}
+        width={imgWidth > 960 ? imgWidth / 2 : imgWidth}
+        className="max-img">
+        <img src={maImgUrl} style={{ width: '100%', height: '100%' }} alt="" />
+      </Modal>
+    </div>
+  );
+};
