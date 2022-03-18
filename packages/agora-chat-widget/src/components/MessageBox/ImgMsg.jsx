43d88d9f19c6a84649185a4cb2a6a9b4bf2dde34
lizg <903504686@qq.com>
2022-03-18 16:52:55 +0800

    fix: [APAAS-2158] img

--- packages/agora-chat-widget/src/components/MessageBox/ImgMsg.jsx
+++ packages/agora-chat-widget/src/components/MessageBox/ImgMsg.jsx
@@ -67,9 +67,19 @@ export const ImgMsg = ({ item }) => {
         visible={maxImg}
         footer={null}
         width={imgWidth}
-        bodyStyle={{ padding: '0' }}
+        closable={false}
+        bodyStyle={{
+          padding: '0',
+          display: 'flex',
+          justifyContent: 'center',
+        }}
         className="max-img">
-        <img src={maxImgUrl} style={{ width: '100%', height: '100%' }} alt="" />
+        <div className="img-box">
+          <img src={maxImgUrl} style={{ maxHeight: '720px' }} alt="" />
+          <div className="closeStyle" onClick={() => setMaxImg(false)}>
+            X
+          </div>
+        </div>
       </Modal>
     </div>
   );
