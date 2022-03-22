c84678e2850856246fb22e0ced5867ebf03c9509
qianfengg <fred_gqf0221@163.com>
2022-03-22 17:51:51 +0800

    fix: color

--- packages/agora-chat-widget/src/components/Button/index.tsx
+++ packages/agora-chat-widget/src/components/Button/index.tsx
@@ -5,12 +5,14 @@ import './index.css';
 
 type ButtonType = 'primary' | 'secondary' | 'ghost' | 'danger';
 
+const primaryColor = '#ddb332';
+
 function createRipple(container: HTMLDivElement, y: number, x: number, type: ButtonType) {
   const circleElement = document.createElement('div');
   circleElement.className = 'ripple-circle';
   circleElement.style.top = y + 'px';
   circleElement.style.left = x + 'px';
-  circleElement.style.background = type === 'primary' ? 'rgba(38, 99, 208, 1)' : '#fff';
+  circleElement.style.background = type === 'primary' ? primaryColor : '#fff';
   container.appendChild(circleElement);
   setTimeout(() => circleElement.remove(), 900);
 }
