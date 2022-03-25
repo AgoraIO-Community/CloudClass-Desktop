9aa30c80b05f277a4928fc5bd0ae4c472c28d8a5
qianfengg <fred_gqf0221@163.com>
2022-03-25 15:24:38 +0800

    fix: answer width

--- packages/agora-plugin-gallery/src/gallery/answer/index.tsx
+++ packages/agora-plugin-gallery/src/gallery/answer/index.tsx
@@ -20,7 +20,7 @@ export class AgoraSelector implements IAgoraExtensionApp {
   appIdentifier = SELECTOR;
   appName = transI18n('widget_selector.appName');
   icon = 'answer';
-  width = 360;
+  width = 370;
   height = 150; // 超过4个选项高度为220
   minWidth = 360;
   minHeight = 150; // 超过4个选项高度为220
