ce05a380f45c204b9a09c4a1f9a3e0398053b00e
renyifei <renyifei@agora.io>
2022-04-20 16:54:44 +0800

    fix: upgrade window manager version to 0.4.23

--- packages/agora-classroom-sdk/webpack/utils/index.js
+++ packages/agora-classroom-sdk/webpack/utils/index.js
@@ -1,10 +1,26 @@
 const path = require('path');
-
+const fs = require('fs');
 const DIST_PATH = path.resolve(__dirname, '../../', 'dist');
 const SRC_PATH = path.resolve(__dirname, '../../', 'src');
 const PUBLIC_PATH = path.resolve(__dirname, '../../', 'public');
 const ROOT_PATH = path.resolve(__dirname, '../../');
 const DEFAULT_PORT = 3000;
+
+const libs = ['agora-rte-sdk', 'agora-edu-core'];
+
+const ALIAS = libs.reduce((prev, cur) => {
+  const libName = cur;
+
+  const libPath = path.resolve(ROOT_PATH, `../${libName}/src`);
+
+  const libExists = fs.existsSync(libPath);
+
+  if (libExists) {
+    prev[libName] = libPath;
+  }
+
+  return prev;
+}, {});
 
 module.exports = {
   DIST_PATH,
@@ -12,4 +28,5 @@ module.exports = {
   PUBLIC_PATH,
   ROOT_PATH,
   DEFAULT_PORT,
+  ALIAS,
 };
