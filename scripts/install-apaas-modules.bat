406f43663ba5af16ab62156505f774925d0f9df8
renyifei <renyifei@agora.io>
2022-04-20 11:49:38 +0800

    fix: windows ci build sciprt

--- scripts/install-apaas-modules.bat
+++ scripts/install-apaas-modules.bat
@@ -4,7 +4,7 @@ call yarn install
 
 set bin_path=node_modules\.bin
 
-call %bin_path%\lerna exec --scope=agora-rte-sdk 'npm run build && npm run build:types'
-call %bin_path%\lerna exec --scope=agora-edu-core 'npm run proto && npm run build && npm run build:types'
+call %bin_path%\lerna exec --scope=agora-rte-sdk "npm run build && npm run build:types"
+call %bin_path%\lerna exec --scope=agora-edu-core "npm run proto && npm run build && npm run build:types"
 
 call yarn bootstrap
