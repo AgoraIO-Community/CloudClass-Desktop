83d0b93e4675c4c3bd048ff6dc1fd704ba59eaaf
任一飞 <renyifei@agora.io>
2022-04-20 02:51:25 +0000

    Merge pull request #1554 in ADUC/cloudclass-desktop from fix/big-stream-resolution to release/apaas/2.4.0-2
    Squashed commit of the following:
    
    commit ce9a64cf6e734d32bdd6a71bb5e61cfea554f6dc
    Author: renyifei <renyifei@agora.io>
    Date:   Wed Apr 20 10:36:19 2022 +0800
    
        fix: windows ci script & default stream resolution

--- scripts/install-apaas-modules.bat
+++ scripts/install-apaas-modules.bat
@@ -4,7 +4,7 @@ call yarn install
 
 set bin_path=node_modules\.bin
 
-call %bin_path%\lerna exec --scope=agora-rte-sdk 'yarn build && yarn build:types'
-call %bin_path%\lerna exec --scope=agora-edu-core 'yarn proto && yarn build && yarn build:types'
+call %bin_path%\lerna exec --scope=agora-rte-sdk 'call yarn build && call yarn build:types'
+call %bin_path%\lerna exec --scope=agora-edu-core 'call yarn proto && call yarn build && call yarn build:types'
 
 call yarn bootstrap
