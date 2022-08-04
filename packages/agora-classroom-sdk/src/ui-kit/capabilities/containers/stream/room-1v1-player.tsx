6c9861d7c837c065e55c0970e516f064e7c24a6a
任一飞 <renyifei@agora.io>
2022-08-04 03:04:34 +0000

    Merge pull request #1776 in ADUC/cloudclass-desktop from jira/APAAS-3341 to release/2.7.0
    Squashed commit of the following:
    
    commit 927092f9be49b6025530cadffc694d9fc9aa8f15
    Author: zhanheng <zhanheng@agora.io>
    Date:   Thu Aug 4 10:28:50 2022 +0800
    
        fix: [APAAS-3379]
    
    commit 5459ba16ac3689e673e65d21925f80ff02af6e53
    Author: zhanheng <zhanheng@agora.io>
    Date:   Thu Aug 4 10:20:31 2022 +0800
    
        fix: [APAAS-3341]

--- packages/agora-classroom-sdk/src/ui-kit/capabilities/containers/stream/room-1v1-player.tsx
+++ packages/agora-classroom-sdk/src/ui-kit/capabilities/containers/stream/room-1v1-player.tsx
@@ -4,6 +4,8 @@ import { EduRoleTypeEnum } from 'agora-edu-core';
 import { observer } from 'mobx-react';
 import React from 'react';
 import { StreamPlaceholder } from '.';
+import { visibilityControl } from '../visibility';
+import { studentVideoEnabled, teacherVideoEnabled } from '../visibility/controlled';
 import { DragableStream } from './draggable-stream';
 
 export const Room1v1StreamsContainer = () => {
@@ -17,48 +19,65 @@ export const Room1v1StreamsContainer = () => {
   );
 };
 
-export const TeacherStream = observer(() => {
-  const { streamUIStore } = useStore() as Edu1v1ClassUIStore;
-  const { teacherCameraStream, videoStreamSize } = streamUIStore;
+export const TeacherStream = visibilityControl(
+  observer(() => {
+    const { streamUIStore } = useStore() as Edu1v1ClassUIStore;
+    const { teacherCameraStream, videoStreamSize } = streamUIStore;
 
-  const style = {
-    marginTop: 2,
-    width: videoStreamSize.width,
-    height: videoStreamSize.height,
-  }
+    const style = {
+      marginTop: 2,
+      width: videoStreamSize.width,
+      height: videoStreamSize.height,
+    };
 
-  const playerStyle = {
-    width: videoStreamSize.width,
-    height: videoStreamSize.height,
-  }
+    const playerStyle = {
+      width: videoStreamSize.width,
+      height: videoStreamSize.height,
+    };
 
-  return (
-    <React.Fragment>
-      <DragableStream stream={teacherCameraStream} style={style} playerStyle={playerStyle} />
-      {!teacherCameraStream && <StreamPlaceholder className='fcr-stream-player-container' role={EduRoleTypeEnum.teacher} style={style} />}
-    </React.Fragment>
+    return (
+      <React.Fragment>
+        <DragableStream stream={teacherCameraStream} style={style} playerStyle={playerStyle} />
+        {!teacherCameraStream && (
+          <StreamPlaceholder
+            className="fcr-stream-player-container"
+            role={EduRoleTypeEnum.teacher}
+            style={style}
+          />
+        )}
+      </React.Fragment>
+    );
+  }),
+  teacherVideoEnabled,
+);
 
-  );
-});
+export const StudentStream = visibilityControl(
+  observer(() => {
+    const { streamUIStore } = useStore() as Edu1v1ClassUIStore;
+    const { studentCameraStream, videoStreamSize } = streamUIStore;
 
-export const StudentStream = observer(() => {
-  const { streamUIStore } = useStore() as Edu1v1ClassUIStore;
-  const { studentCameraStream, videoStreamSize } = streamUIStore;
+    const style = {
+      marginTop: 2,
+      width: videoStreamSize.width,
+      height: videoStreamSize.height,
+    };
 
-  const style = {
-    marginTop: 2,
-    width: videoStreamSize.width,
-    height: videoStreamSize.height,
-  }
-
-  const playerStyle = {
-    width: videoStreamSize.width,
-    height: videoStreamSize.height,
-  }
-  return (
-    <React.Fragment>
-      <DragableStream stream={studentCameraStream} style={style} playerStyle={playerStyle} />
-      {!studentCameraStream && <StreamPlaceholder className='fcr-stream-player-container' role={EduRoleTypeEnum.student} style={style} />}
-    </React.Fragment>
-  );
-});
+    const playerStyle = {
+      width: videoStreamSize.width,
+      height: videoStreamSize.height,
+    };
+    return (
+      <React.Fragment>
+        <DragableStream stream={studentCameraStream} style={style} playerStyle={playerStyle} />
+        {!studentCameraStream && (
+          <StreamPlaceholder
+            className="fcr-stream-player-container"
+            role={EduRoleTypeEnum.student}
+            style={style}
+          />
+        )}
+      </React.Fragment>
+    );
+  }),
+  studentVideoEnabled,
+);
