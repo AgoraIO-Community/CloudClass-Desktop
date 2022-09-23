5bf7b357481b71b7c8058c9d9e5eabd67112fa2a
任一飞 <renyifei@agora.io>
2022-09-23 09:10:07 +0000

    Merge pull request #1815 in ADUC/cloudclass-desktop from feat/2.8.0_i18n_ele to release/2.8.0
    Squashed commit of the following:
    
    commit 2977e31d8532f9ec4d80f14d9d91c75fe5941b25
    Merge: 94df87d61 73770c340
    Author: Claus <monkeydreamgo@gmail.com>
    Date:   Fri Sep 23 17:08:24 2022 +0800
    
        fix: merge release 2.8.0
    
    commit 94df87d6194adbaff005d5bbd430f3324d456530
    Author: Claus <monkeydreamgo@gmail.com>
    Date:   Wed Sep 21 17:33:25 2022 +0800
    
        feat: update h5 logo
    
    commit d953a7fd114efa17f60ecb3cc663dec88abf09df
    Author: Claus <monkeydreamgo@gmail.com>
    Date:   Wed Sep 21 16:27:37 2022 +0800
    
        feat: replace logo
    
    commit f74fc5a2cefad3899d7d8fba3ff36045098ffe4a
    Author: Claus <monkeydreamgo@gmail.com>
    Date:   Wed Sep 21 15:42:21 2022 +0800
    
        feat: 优化根据语言渲染元素函数

--- packages/agora-classroom-sdk/src/app/layout/home-layout/index.tsx
+++ packages/agora-classroom-sdk/src/app/layout/home-layout/index.tsx
@@ -1,33 +1,34 @@
 import { useHomeStore } from '@/app/hooks';
+import { useElementWithI18n } from '@/app/hooks/useComWithI18n';
 import { observer } from 'mobx-react';
-import { FC, PropsWithChildren, useMemo } from 'react';
+import { FC, PropsWithChildren } from 'react';
 import { getI18n } from 'react-i18next';
 import { ASpin, useI18n } from '~ui-kit';
 import './index.css';
 
 export const HomeLayout: FC<PropsWithChildren<unknown>> = observer(({ children }) => {
   const transI18n = useI18n();
-  const i18n = getI18n();
   const homeStore = useHomeStore();
-  const slogan = useMemo(() => {
-    if (i18n.language === 'en') {
-      return (
-        <div className="text">
-          BUILD
-          <br />
-          ONLINE CLASSROOM
-          <br />
-          IN MINUTES
-        </div>
-      );
-    }
-    return (
+  const i18n = getI18n();
+
+  const slogan = useElementWithI18n({
+    en: (
+      <div className="text">
+        BUILD
+        <br />
+        ONLINE CLASSROOM
+        <br />
+        IN MINUTES
+      </div>
+    ),
+    zh: (
       <div className="text">
         轻松创建
         <br /> 线上专属课堂
       </div>
-    );
-  }, [i18n.language]);
+    ),
+  });
+
   return (
     <ASpin
       tip={transI18n('fcr_loading')}
@@ -38,7 +39,7 @@ export const HomeLayout: FC<PropsWithChildren<unknown>> = observer(({ children }
         <div className={`home`}>
           <div className="home-left">
             <header className="flex items-center">
-              <div className="logo">{transI18n('fcr_home_label_logo')}</div>
+              <div className={`logo ${i18n.language}`}></div>
               {/* <div>Product</div> */}
             </header>
             {slogan}
