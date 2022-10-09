fdd2d02811c07fc3cda90ecbb4de25e5a7ac34ab
李志兵 <lizhibing@agora.io>
2022-10-09 06:29:37 +0000

    Merge pull request #1831 in ADUC/cloudclass-desktop from feature/study-room-1 to feature/study-room
    Squashed commit of the following:
    
    commit a27ed28520e9b2c2d29db4c9e88302f98ce07fb3
    Author: jason_lee <lizhibing@agora.io>
    Date:   Fri Sep 30 18:05:31 2022 +0800
    
        feat: mic button

--- packages/agora-classroom-sdk/src/ui-kit/capabilities/containers-v2/hocs/index.tsx
+++ packages/agora-classroom-sdk/src/ui-kit/capabilities/containers-v2/hocs/index.tsx
@@ -11,233 +11,277 @@ import { IconButton } from '~components-v2';
 import { getColorByLevel } from '~utilities/palette-helper';
 import './index.css';
 
-
 type DeviceDropdownProps = {
-    disabled: boolean, muted: boolean, icon: SvgIconEnum,
-    onDeviceClick: () => void;
-    dropdownContent: React.ReactNode;
+  disabled: boolean;
+  muted: boolean;
+  icon: SvgIconEnum;
+  onDeviceClick: () => void;
+  dropdownContent: React.ReactNode;
 };
 
-const DeviceDropdown = observer(({ disabled, muted, icon, onDeviceClick, dropdownContent }: DeviceDropdownProps) => {
+const DeviceDropdown = observer(
+  ({ disabled, muted, icon, onDeviceClick, dropdownContent }: DeviceDropdownProps) => {
     const [dropdownPanelVisible, setDropdownPanelVisible] = useState(false);
     const [hover, setHover] = useState(false);
     const [focused, setFocused] = useState(false);
     const opacity = disabled ? 0.5 : 1;
     const backgroundColor = muted ? '#F5655C' : '#18191B';
 
-    const dropdownPanelCls = classNames("fcr-device-dropdown__panel absolute", {
-        'hidden': !dropdownPanelVisible
+    const dropdownPanelCls = classNames('fcr-device-dropdown__panel absolute', {
+      hidden: !dropdownPanelVisible,
     });
 
     const arrowButtonBackground = useMemo(() => {
-        if (hover) {
-            return getColorByLevel(backgroundColor, 7);
-        }
+      if (hover) {
+        return getColorByLevel(backgroundColor, 7);
+      }
 
-        if (focused) {
-            return getColorByLevel(backgroundColor, 8)
-        }
-        return backgroundColor;
+      if (focused) {
+        return getColorByLevel(backgroundColor, 8);
+      }
+      return backgroundColor;
     }, [backgroundColor, hover, focused]);
 
-
     const preventClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
-        e.preventDefault();
-    }
+      e.preventDefault();
+    };
 
     const handleBlur = () => {
-        setFocused(false);
-        setDropdownPanelVisible(false);
-    }
+      setFocused(false);
+      setDropdownPanelVisible(false);
+    };
 
     const handleFocus = () => {
-        setFocused(true);
-        setDropdownPanelVisible(true);
-    }
+      setFocused(true);
+      setDropdownPanelVisible(true);
+    };
 
     return (
-        <div className='fcr-device-dropdown relative'>
-            <IconButton
-                icon={icon}
-                iconColor={"#fff"}
-                backgroundColor={backgroundColor}
-                opacity={opacity}
-                disabled={disabled}
-                tailSlot={
-                    !disabled && <SvgImg type={SvgIconEnum.DOWN} size={9} className="invisible mx-1" />
-                }
-                transition='background-color .2s'
-                onClick={onDeviceClick}
+      <div className="fcr-device-dropdown relative">
+        <IconButton
+          icon={icon}
+          iconColor={'#fff'}
+          backgroundColor={backgroundColor}
+          opacity={opacity}
+          disabled={disabled}
+          tailSlot={
+            !disabled && <SvgImg type={SvgIconEnum.DOWN} size={9} className="invisible mx-1" />
+          }
+          transition="background-color .2s"
+          onClick={onDeviceClick}
+        />
+        {/* arrow button */}
+        {!disabled && (
+          <a
+            href="#"
+            onMouseEnter={() => setHover(true)}
+            onMouseLeave={() => setHover(false)}
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+            className="fcr-device-dropdown__arrow-btn absolute flex items-center cursor-pointer"
+            style={{
+              right: 4,
+              top: '50%',
+              height: '70%',
+              transform: 'translateY(-50%)',
+              borderRadius: 6,
+              padding: '0 5px',
+              background: arrowButtonBackground,
+              transition: 'background-color .2s',
+            }}
+            onClick={preventClick}>
+            <SvgImg
+              type={SvgIconEnum.DOWN}
+              colors={{ iconPrimary: '#fff' }}
+              size={9}
+              style={{ transition: 'all 0.2s ease 0s', transform: 'rotate(180deg)' }}
             />
-            {/* arrow button */}
-            {
-                !disabled && <a
-                    href="#"
-                    onMouseEnter={() => setHover(true)}
-                    onMouseLeave={() => setHover(false)}
-                    onFocus={handleFocus}
-                    onBlur={handleBlur}
-                    className="fcr-device-dropdown__arrow-btn absolute flex items-center cursor-pointer"
-                    style={{ right: 4, top: '50%', height: '70%', transform: 'translateY(-50%)', borderRadius: 6, padding: '0 5px', background: arrowButtonBackground, transition: 'background-color .2s' }}
-                    onClick={preventClick}
-                >
-                    <SvgImg
-                        type={SvgIconEnum.DOWN}
-                        colors={{ iconPrimary: '#fff' }}
-                        size={9}
-                        style={{ transition: 'all 0.2s ease 0s', transform: 'rotate(180deg)' }}
-                    />
-                </a>
-            }
-            {/* dropdown panel */}
-            <div
-                className={dropdownPanelCls}
-                style={{
-                    width: 250,
-                    background: '#21212B',
-                    borderRadius: 16,
-                    bottom: 'calc(100% + 24px)'
-                }}
-                onClick={
-                    () => {
-                        setDropdownPanelVisible(false);
-                    }
-                }
-            >
-                {dropdownContent}
-            </div>
-        </div >
+          </a>
+        )}
+        {/* dropdown panel */}
+        <div
+          className={dropdownPanelCls}
+          style={{
+            width: 250,
+            background: '#21212B',
+            borderRadius: 16,
+            bottom: 'calc(100% + 24px)',
+          }}
+          onClick={() => {
+            setDropdownPanelVisible(false);
+          }}>
+          {dropdownContent}
+        </div>
+      </div>
     );
-});
+  },
+);
 
 export const CameraDropdown = observer(() => {
-    const { navigationBarUIStore, pretestUIStore } = useStore();
-    const { localCameraOff, toggleLocalVideo } = navigationBarUIStore;
-    const { cameraDevicesList, setCameraDevice, currentCameraDeviceId } = pretestUIStore;
-    const disabled = false;
-    const icon = localCameraOff ? SvgIconEnum.CAMERA_OFF : SvgIconEnum.CAMERA_ON;
+  const { navigationBarUIStore, pretestUIStore } = useStore();
+  const { localCameraOff, toggleLocalVideo } = navigationBarUIStore;
+  const { cameraDevicesList, setCameraDevice, currentCameraDeviceId } = pretestUIStore;
+  const disabled = false;
+  const icon = localCameraOff ? SvgIconEnum.CAMERA_OFF : SvgIconEnum.CAMERA_ON;
 
-    const handleDeviceClick = () => {
-        toggleLocalVideo();
-    }
+  const handleDeviceClick = () => {
+    toggleLocalVideo();
+  };
 
-    const content = (
-        <React.Fragment>
-            <span className='font-bold px-2'>Camera</span>
-            {
-                cameraDevicesList.map(({ label, value }) => {
-                    const handleSelect = () => {
-                        setCameraDevice(value);
-                    }
+  const content = (
+    <React.Fragment>
+      <span className="font-bold px-2">Camera</span>
+      {cameraDevicesList.map(({ label, value }) => {
+        const handleSelect = () => {
+          setCameraDevice(value);
+        };
 
-                    return (
-                        <div key={value} onMouseDown={handleSelect} className="fcr-device-dropdown__panel-item flex justify-between p-2 my-1 cursor-pointer">
-                            <span>{label}</span>
-                            {
-                                currentCameraDeviceId === value && <SvgImg type={SvgIconEnum.CHECK} colors={{ iconPrimary: '#fff' }} />
-                            }
-                        </div>
-                    );
-                })
-            }
-        </React.Fragment>
-    );
+        return (
+          <div
+            key={value}
+            onMouseDown={handleSelect}
+            className="fcr-device-dropdown__panel-item flex justify-between p-2 my-1 cursor-pointer">
+            <span>{label}</span>
+            {currentCameraDeviceId === value && (
+              <SvgImg type={SvgIconEnum.CHECK} colors={{ iconPrimary: '#fff' }} />
+            )}
+          </div>
+        );
+      })}
+    </React.Fragment>
+  );
 
-    return <DeviceDropdown disabled={disabled} muted={localCameraOff} icon={icon} onDeviceClick={handleDeviceClick} dropdownContent={content} />;
+  return (
+    <DeviceDropdown
+      disabled={disabled}
+      muted={localCameraOff}
+      icon={icon}
+      onDeviceClick={handleDeviceClick}
+      dropdownContent={content}
+    />
+  );
 });
 
-
 export const MicDropdown = observer(() => {
-    const { navigationBarUIStore, pretestUIStore } = useStore();
-    const { localMicOff, toggleLocalAudio } = navigationBarUIStore;
-    const { recordingDevicesList, setRecordingDevice, currentRecordingDeviceId } = pretestUIStore;
-    const disabled = true;
-    const icon = localMicOff ? SvgIconEnum.MIC_OFF : SvgIconEnum.MIC_ON;
+  const { navigationBarUIStore, pretestUIStore } = useStore();
+  const { localMicOff, toggleLocalAudio } = navigationBarUIStore;
+  const { recordingDevicesList, setRecordingDevice, currentRecordingDeviceId } = pretestUIStore;
+  const disabled = true;
+  const icon = localMicOff ? SvgIconEnum.MIC_OFF : SvgIconEnum.MIC_ON;
 
-    const handleDeviceClick = () => {
-        toggleLocalAudio();
-    }
+  const handleDeviceClick = () => {
+    // toggleLocalAudio();
+  };
 
-    const content = (
-        <React.Fragment>
-            <span className='font-bold px-2'>Microphone</span>
-            {
-                recordingDevicesList.map(({ label, value }) => {
-                    const handleSelect = () => {
-                        setRecordingDevice(value);
-                    }
+  const content = (
+    <React.Fragment>
+      <span className="font-bold px-2">Microphone</span>
+      {recordingDevicesList.map(({ label, value }) => {
+        const handleSelect = () => {
+          setRecordingDevice(value);
+        };
 
-                    return (
-                        <div key={value} onMouseDown={handleSelect} className="fcr-device-dropdown__panel-item flex justify-between p-2 my-1 cursor-pointer">
-                            <span>{label}</span>
-                            {
-                                currentRecordingDeviceId === value && <SvgImg type={SvgIconEnum.CHECK} colors={{ iconPrimary: '#fff' }} />
-                            }
-                        </div>
-                    );
-                })
-            }
-        </React.Fragment>
-    );
+        return (
+          <div
+            key={value}
+            onMouseDown={handleSelect}
+            className="fcr-device-dropdown__panel-item flex justify-between p-2 my-1 cursor-pointer">
+            <span>{label}</span>
+            {currentRecordingDeviceId === value && (
+              <SvgImg type={SvgIconEnum.CHECK} colors={{ iconPrimary: '#fff' }} />
+            )}
+          </div>
+        );
+      })}
+    </React.Fragment>
+  );
 
-
-    return <DeviceDropdown disabled={disabled} muted={localMicOff} icon={icon} onDeviceClick={handleDeviceClick} dropdownContent={content} />;
+  return (
+    <DeviceDropdown
+      disabled={disabled}
+      muted={localMicOff}
+      icon={icon}
+      onDeviceClick={handleDeviceClick}
+      dropdownContent={content}
+    />
+  );
 });
 
 export const ScreenShareTool = observer(() => {
-    const { classroomStore } = useStore();
+  const { classroomStore } = useStore();
 
-    const handleClick = () => {
-        if (classroomStore.mediaStore.localScreenShareTrackState !== AgoraRteMediaSourceState.started) {
-            classroomStore.mediaStore.startScreenShareCapture();
-        } else {
-            classroomStore.mediaStore.stopScreenShareCapture();
-        }
+  const handleClick = () => {
+    if (classroomStore.mediaStore.localScreenShareTrackState !== AgoraRteMediaSourceState.started) {
+      classroomStore.mediaStore.startScreenShareCapture();
+    } else {
+      classroomStore.mediaStore.stopScreenShareCapture();
     }
+  };
 
-    return <IconButton iconColor='#fff' backgroundColor={'#343434E5'} icon={SvgIconEnum.SCREENSHARING} onClick={handleClick} />;
+  return (
+    <IconButton
+      iconColor="#fff"
+      backgroundColor={'#343434E5'}
+      icon={SvgIconEnum.SCREENSHARING}
+      onClick={handleClick}
+    />
+  );
 });
-
 
 export const ChatTool = observer(() => {
-
-    return (
-        <div className="">
-            {/* <Chat /> */}
-            <IconButton iconColor='#fff' backgroundColor={'#343434E5'} icon={SvgIconEnum.COLORED_CHAT} />
-        </div>
-    );
+  return (
+    <div className="">
+      {/* <Chat /> */}
+      <IconButton iconColor="#fff" backgroundColor={'#343434E5'} icon={SvgIconEnum.COLORED_CHAT} />
+    </div>
+  );
 });
 
-
 export const RosterTool = observer(() => {
-    const { toolbarUIStore, streamUIStore } = useStore() as EduStudyRoomUIStore;
+  const { toolbarUIStore, streamUIStore } = useStore() as EduStudyRoomUIStore;
 
+  const handleClick = () => {
+    toolbarUIStore.setTool('register');
+  };
 
-    const handleClick = () => {
-        toolbarUIStore.setTool('register');
-    }
-
-
-    return (
-        <div className='relative overflow-hidden'>
-            <div className='text-center w-full bottom-0 absolute' style={{ background: 'rgba(52, 52, 52, 0.9)', borderRadius: 12, fontSize: 16, lineHeight: '20px', pointerEvents: 'none' }}>
-                {streamUIStore.orderedUserList.length > 999 ? 999 : streamUIStore.orderedUserList.length}
-            </div>
-            <IconButton iconColor='#fff' backgroundColor={'#343434E5'} icon={SvgIconEnum.PEOPLE} onClick={handleClick} />
-        </div>
-    );
+  return (
+    <div className="relative overflow-hidden">
+      <div
+        className="text-center w-full bottom-0 absolute"
+        style={{
+          background: 'rgba(52, 52, 52, 0.9)',
+          borderRadius: 12,
+          fontSize: 16,
+          lineHeight: '20px',
+          pointerEvents: 'none',
+        }}>
+        {streamUIStore.orderedUserList.length > 999 ? 999 : streamUIStore.orderedUserList.length}
+      </div>
+      <IconButton
+        iconColor="#fff"
+        backgroundColor={'#343434E5'}
+        icon={SvgIconEnum.PEOPLE}
+        onClick={handleClick}
+      />
+    </div>
+  );
 });
 
 export const Quit = observer(() => {
-    const { shareUIStore, classroomStore } = useStore();
-    const handleClick = () => {
-        shareUIStore.addDialog(DialogCategory.Quit, {
-            onOk: () => {
-                classroomStore.connectionStore.leaveClassroom(LeaveReason.leave);
-            },
-        });
-    }
-    return <IconButton iconColor='#fff' backgroundColor={'#F5655C'} icon={SvgIconEnum.QUIT} onClick={handleClick} />;
+  const { shareUIStore, classroomStore } = useStore();
+  const handleClick = () => {
+    shareUIStore.addDialog(DialogCategory.Quit, {
+      onOk: () => {
+        classroomStore.connectionStore.leaveClassroom(LeaveReason.leave);
+      },
+    });
+  };
+  return (
+    <IconButton
+      iconColor="#fff"
+      backgroundColor={'#F5655C'}
+      icon={SvgIconEnum.QUIT}
+      onClick={handleClick}
+    />
+  );
 });
-
