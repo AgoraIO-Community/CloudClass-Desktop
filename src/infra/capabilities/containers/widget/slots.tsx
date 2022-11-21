bc31e411cafd9f7ae7f4a6e8c0f47848e580fee2
湛恒 <zhanheng@agora.io>
2022-11-21 08:37:35 +0000

    Merge pull request #1858 in ADUC/cloudclass-desktop from fix/remove-rtm-chat to release/2.8.0
    Squashed commit of the following:
    
    commit 31222d8c2e2dd4a5c6a79ebf9763a938643044a7
    Author: renyifei <renyifei@agora.io>
    Date:   Mon Nov 21 16:35:18 2022 +0800
    
        fix: remove rtm chat

--- src/infra/capabilities/containers/widget/slots.tsx
+++ src/infra/capabilities/containers/widget/slots.tsx
@@ -14,10 +14,7 @@ export const Chat = observer(function Chat() {
 
   useEffect(() => {
     if (ready) {
-      const chatWidgetId =
-        EduClassroomConfig.shared.rteEngineConfig.region === EduRegion.CN
-          ? 'easemobIM'
-          : 'io.agora.widget.chat';
+      const chatWidgetId = 'easemobIM';
 
       if (ready) {
         widgetUIStore.createWidget(chatWidgetId);
