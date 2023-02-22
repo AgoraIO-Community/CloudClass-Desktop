7d08d5c9c954ff672b95715e577cdbb80d4c2007
任一飞 <renyifei@agora.io>
2023-02-22 13:35:40 +0000

    Merge pull request #1936 in ADUC/cloudclass-desktop from fix/rtc-ext to release/2.8.20
    Squashed commit of the following:
    
    commit 89d910a3f0ba7d40fba18ab77f9b6303cfe12ac3
    Author: zhanheng <zhanheng@agora.io>
    Date:   Wed Feb 22 21:34:00 2023 +0800
    
        fix: disabled rtc ext when record

--- src/infra/stores/common/pretest/index.ts
+++ src/infra/stores/common/pretest/index.ts
@@ -14,6 +14,7 @@ import {
   DEVICE_DISABLE,
   EduClassroomConfig,
   EduEventCenter,
+  EduRoleTypeEnum,
   EduRteEngineConfig,
   EduRteRuntimePlatform,
   Platform,
@@ -65,7 +66,8 @@ export class PretestUIStore extends EduUIStoreBase {
         () =>
           this.classroomStore.connectionStore.engine &&
           EduRteEngineConfig.platform === EduRteRuntimePlatform.Web &&
-          EduClassroomConfig.shared.platform !== Platform.H5,
+          EduClassroomConfig.shared.platform !== Platform.H5 &&
+          EduClassroomConfig.shared.sessionInfo.role !== EduRoleTypeEnum.invisible,
         (processorsRequired) => {
           if (processorsRequired) {
             getProcessorInitializer<IVirtualBackgroundProcessor>(
