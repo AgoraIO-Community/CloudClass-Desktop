cc6c7a2514ee15d90dec8fe2603605a0f630dc07
zhanheng <zhanheng@agora.io>
2023-06-15 11:37:11 +0800

    feat: ci script

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const webpackMerge = require('webpack-merge');
 const path = require('path');
 const baseConfig = require('agora-common-libs/presets/webpack.config.base.js');
 const ROOT_PATH = path.resolve(__dirname, './');
-const CopyPlugin = require('copy-webpack-plugin');
 const config = {
   entry: {
     edu_sdk: './src/infra/api/index.tsx',
@@ -20,31 +19,6 @@ const config = {
       'agora-classroom-sdk': path.resolve(ROOT_PATH, './src/infra/api'),
     },
   },
-
-  plugins: [
-    new CopyPlugin({
-      patterns: [
-        // ai denoiser
-        {
-          from: path.resolve(ROOT_PATH, '../../node_modules/agora-extension-ai-denoiser/external'),
-          to: path.resolve(ROOT_PATH, './lib/externals/extensions/ai-denoiser'),
-          noErrorOnMissing: true,
-        },
-        //virtual background
-        {
-          from: path.resolve(
-            ROOT_PATH,
-            '../../node_modules/agora-extension-virtual-background/wasms',
-          ),
-          to: path.resolve(
-            ROOT_PATH,
-            './lib/externals/extensions/agora-extension-virtual-background',
-          ),
-          noErrorOnMissing: true,
-        },
-      ],
-    }),
-  ],
 };
 
 const mergedConfig = webpackMerge.merge(baseConfig, config);
