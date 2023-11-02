289ebcc45d9ce63fdc30f45ab3fde57a926d381c
任一飞 <renyifei@agora.io>
2023-11-02 06:18:50 +0000

    Merge pull request #1990 in ADUC/cloudclass-desktop from feat/update-2.9.2 to release/2.9.2
    Squashed commit of the following:
    
    commit 2b4e7a730ca2ec15b754f0b2f107deb122ba637f
    Author: zhanheng <zhanheng@agora.io>
    Date:   Thu Nov 2 14:14:43 2023 +0800
    
        feat: update version

--- ci/build/build_mac.sh
+++ ci/build/build_mac.sh
@@ -9,14 +9,14 @@ lib_dependencies=(
     agora-common-libs
 )
 lib_versions=(
-    2.9.1
-    2.9.1
-    2.9.3
+    2.9.2
+    2.9.2
+    2.9.4
 )
 lib_branches=(
-    release/2.9.1
-    release/2.9.1
-    release/2.9.3
+    release/2.9.2
+    release/2.9.2
+    release/2.9.4
 )
 
 . ../apaas-cicd-web/utilities/tools.sh
