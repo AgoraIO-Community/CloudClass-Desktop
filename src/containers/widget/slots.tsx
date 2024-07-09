56f8fd5a5017d734d3ba4e4a1881497f30d1bada
yaoweiyu <493449664@qq.com>
2024-07-09 13:51:23 +0800

    fix: 表情样式修改

--- src/containers/widget/slots.tsx
+++ src/containers/widget/slots.tsx
@@ -323,7 +323,7 @@ export const Chat = observer(function Chat() {
         flexShrink: 0,
         background: isLandscape ? 'transparent' : '#27292f',
         overflow: 'hidden',
-        zIndex: ComponentLevelRules.Level1,
+        zIndex: ComponentLevelRules.Level3,
       }}
     />
   );
